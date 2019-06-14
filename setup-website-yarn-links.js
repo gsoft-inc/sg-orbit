1740cd81ba428d053f79d352dc898a080f4c6ada
patrick.lafrance <patrick.lafrance@outlook.com>
2019-06-14 17:10:20 -0400

    New setup

---
+++ setup-website-yarn-links.js
@@ -0,0 +1,54 @@
+const chalk = require("chalk");
+const shell = require("shelljs");
+
+function run(command, errorMessage) {
+    const result = command();
+
+    if (result.code !== 0) {
+        if (errorMessage) {
+            console.error(chalk.red("error"), ` ${errorMessage}`);
+        }
+
+        if (result.stderr && result.stderr !== "") {
+            console.error(chalk.red("error"), ` ${result.stderr}`);
+        }
+
+        shell.exit(1);
+    }
+
+    return result;
+}
+
+////////////////////////////////////////////////////////
+
+const PROJECTS_TO_LINK = [
+    { name: "@sharegate/react-components", path: "packages/react-components" }
+]
+
+console.log(chalk.bold("Creating yarn links for website project..."));
+
+const home = run(() => shell.pwd()).stdout;
+
+PROJECTS_TO_LINK.forEach((project, index) => {
+    shell.cd(project.path);
+
+    const current = `[${index + 1}/${PROJECTS_TO_LINK.length * 2}]`;
+    const result = run(() => shell.exec("yarn link", { silent: true }), `${current} Failed to create yarn link for project "${project.name}".`);
+
+    run(() => shell.cd(home));
+
+    console.log(`${current} Created yarn link for project "${project.name}"...`);
+});
+
+run(() => shell.cd("website"));
+
+PROJECTS_TO_LINK.forEach((project, index) => {
+    const current = `[${(index + 1) * 2}/${PROJECTS_TO_LINK.length * 2}]`;
+    const result = run(() => shell.exec(`yarn link ${project.name}`, { silent: true }), `${current} Failed to link project "${project.name}".`);
+
+    run(() => shell.cd(home));
+    
+    console.log(`${current} Linked project "${project.name}"...`);
+});
+
+console.log(chalk.green("success"), " website project links created.");
