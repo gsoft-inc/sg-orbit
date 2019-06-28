fbd99c20a743d566b43e17ae85f19010e6f26544
Francis Thibault <francis@officevibe.com>
2019-06-28 08:55:23 -0400

    wip

--- website/src/components/header.js
+++ website/src/components/header.js
@@ -3,50 +3,50 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `var(--marine-500)`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      className="flex"
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+    <header
+        style={{
+            background: `var(--marine-500)`,
+            marginBottom: `1.45rem`,
+        }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          className="mr4"
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+        <div
+            className="flex"
+            style={{
+                margin: `0 auto`,
+                maxWidth: 960,
+                padding: `1.45rem 1.0875rem`,
+            }}
         >
-          {siteTitle}
-        </Link>
-        <div>
-          <Link to="/blog" className="no-underline">
-            <span className="hover-cloud-200 white f7">Blog</span>
-          </Link>
-          <Link to="/docs" className="no-underline ml4">
-            <span className="hover-cloud-200 white f7 no-underline">Docs</span>
-          </Link>
+            <h1 style={{ margin: 0 }}>
+                <Link
+                    className="mr4"
+                    to="/"
+                    style={{
+                        color: `white`,
+                        textDecoration: `none`,
+                    }}
+                >
+                    {siteTitle}
+                </Link>
+                <div>
+                    <Link to="/blog" className="no-underline">
+                        <span className="hover-cloud-200 white f7">Blog</span>
+                    </Link>
+                    <Link to="/docs" className="no-underline ml4">
+                        <span className="hover-cloud-200 white f7 no-underline">Docs</span>
+                    </Link>
+                </div>
+            </h1>
         </div>
-      </h1>
-    </div>
-  </header>
+    </header>
 )
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
+    siteTitle: PropTypes.string,
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+    siteTitle: ``,
 }
 
 export default Header
