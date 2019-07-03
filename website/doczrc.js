a8121da5346f08d2c24c1256a9fb8f99e9ff05da
Francis Thibault <francis@officevibe.com>
2019-07-03 14:49:19 -0400

    cleaning

--- website/doczrc.js
+++ website/doczrc.js
@@ -1,30 +1,34 @@
 export default {
-  title: "ShareGate Components",
-  base: "docs",
-  ignore: ["**/blog/**", "**/components/**", "readme.md"],
-  menu: ["Introduction", "Atoms", "Components"],
-  description: "This is my awesome documentation",
-  codeSandbox: false,
-  linesToScrollEditor: 16,
-  themeConfig: {
-    radii: "4px",
-    colors: {
-      primary: `#566077`,
-    },
-    styles: {
-      h1: `
+    title: "ShareGate Components",
+    base: "docs",
+    ignore: ["**/blog/**", "**/components/**", "readme.md"],
+    menu: ["Introduction", "Foundations", "Components"],
+    description: "This is my awesome documentation",
+    codeSandbox: false,
+    linesToScrollEditor: 16,
+    themeConfig: {
+        radii: "4px",
+        colors: {
+            primary: `#566077`
+        },
+        styles: {
+            h1: `
         font-size: 72px;
         margin-bottom: 16px;
         `,
-      h2: `
+            h2: `
         font-size: 24px;
         margin-bottom: 16px;
         margin-top: 24px;
         `,
-      paragraph: `
+            h3: `
         font-size: 20px;
-        margin-bottom: 24px;
+        margin-bottom: 16px;
         `,
-    },
-  },
-}
+            paragraph: `
+            font-size: 18px;
+            margin-bottom: 12px;
+        `
+        }
+    }
+};
