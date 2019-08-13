f6fe6368788647d4e7f456ad6dbbd6c3b2df901a
Francis Thibault <francis.thibault@gsoft.com>
2019-08-13 16:00:42 -0400

    ztopia

--- website/gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
-require("prismjs/themes/prism-okaidia.css");
-require("@sharegate/css-normalize");
