0a7a880fa24deca7fe348be4ba64dcab2dfc62a3
Francis Thibault <francis.thibault@gsoft.com>
2019-08-14 13:16:50 -0400

    folders

--- website/doczrc.js
+++ website/doczrc.js
@@ -3,10 +3,15 @@ import { css } from "docz-plugin-css";
 export default {
     src: "./src",
     title: "Sharegate Orbit",
+    themeConfig: {
+        colors: {
+            primary: "#566077"
+        }
+    },
     description: "Sharegate Orbit components documentation",
     menu: [
         { name: "Introduction", menu: [] },
-        { name: "Foundations", menu: ["Colours", "Typography", "Shadows"] },
+        { name: "Materials", menu: ["Colours", "Typography", "Spacing", "Border Radius", "Shadows"] },
         { name: "Components", menu: [] }
     ],
     notUseSpecifiers: true,
