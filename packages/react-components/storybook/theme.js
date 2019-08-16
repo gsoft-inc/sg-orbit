8bedcacfc56042c687963f697c1bdd150b0b820d
patrick.lafrance <patrick.lafrance@outlook.com>
2019-08-16 09:26:38 -0400

    Try to fix CI

--- packages/react-components/storybook/theme.js
+++ packages/react-components/storybook/theme.js
@@ -3,6 +3,6 @@ import { create } from "@storybook/theming";
 export const customStorybookTheme = create({
     base: "light",
     brandTitle: "Sharegate Orbit",
-    brandUrl: "https://sharegate.com",
-    brandImage: "https://raw.githubusercontent.com/gsoft-inc/sg-brand/master/assets/orbit.svg?sanitize=true"
+    brandUrl: "https://sharegate.com"
+    // brandImage: "https://raw.githubusercontent.com/gsoft-inc/sg-brand/master/assets/orbit.svg?sanitize=true"
 });
