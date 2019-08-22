b4a957a4ee6b6ba6ead8df3f00b649235032376e
Francis Thibault <francis.thibault@gsoft.com>
2019-08-22 11:56:40 -0400

    more specs

---
+++ packages/react-components/stories/semantic-ui-theme/specs/placeholder.specs.stories.jsx
@@ -0,0 +1,83 @@
+import { Placeholder, Segment } from "semantic-ui-react";
+import { storiesBuilder } from "../../../storybook/utils/stories-builder";
+
+function stories(segment) {
+    return storiesBuilder("Semantic-UI-Theme|placeholder")
+        .segment(segment)
+        .layoutWidth("80%")
+        .chromaticDelay(100)
+        .build();
+}
+
+stories()
+    .add("default",
+         () =>
+             <div className="flex flex-column">
+                 <Placeholder className="paused">
+                     <Placeholder.Header image>
+                         <Placeholder.Line />
+                         <Placeholder.Line />
+                     </Placeholder.Header>
+                     <Placeholder.Paragraph>
+                         <Placeholder.Line />
+                         <Placeholder.Line />
+                         <Placeholder.Line />
+                         <Placeholder.Line />
+                     </Placeholder.Paragraph>
+                 </Placeholder>
+                 <Placeholder className="paused">
+                     <Placeholder.Line />
+                     <Placeholder.Line />
+                     <Placeholder.Line />
+                     <Placeholder.Line />
+                     <Placeholder.Line />
+                 </Placeholder>
+                 <Placeholder className="paused">
+                     <Placeholder.Header image>
+                         <Placeholder.Line />
+                         <Placeholder.Line />
+                     </Placeholder.Header>
+                 </Placeholder>
+                 <Placeholder className="paused">
+                     <Placeholder.Paragraph>
+                         <Placeholder.Line />
+                         <Placeholder.Line />
+                         <Placeholder.Line />
+                         <Placeholder.Line />
+                         <Placeholder.Line />
+                     </Placeholder.Paragraph>
+                     <Placeholder.Paragraph>
+                         <Placeholder.Line />
+                         <Placeholder.Line />
+                         <Placeholder.Line />
+                     </Placeholder.Paragraph>
+                 </Placeholder>
+                 <Placeholder className="paused" style={{ height: 150, width: 150 }}>
+                     <Placeholder.Image />
+                 </Placeholder>
+                 <Placeholder className="paused" fluid>
+                     <Placeholder.Header image>
+                         <Placeholder.Line />
+                         <Placeholder.Line />
+                     </Placeholder.Header>
+                     <Placeholder.Paragraph>
+                         <Placeholder.Line />
+                         <Placeholder.Line />
+                         <Placeholder.Line />
+                     </Placeholder.Paragraph>
+                 </Placeholder>
+                 <Segment inverted>
+                     <Placeholder className="paused" inverted>
+                         <Placeholder.Header image>
+                             <Placeholder.Line />
+                             <Placeholder.Line />
+                         </Placeholder.Header>
+                         <Placeholder.Paragraph>
+                             <Placeholder.Line />
+                             <Placeholder.Line />
+                             <Placeholder.Line />
+                         </Placeholder.Paragraph>
+                     </Placeholder>
+                 </Segment>
+             </div>
+    );
