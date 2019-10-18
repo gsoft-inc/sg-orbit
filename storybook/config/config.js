13c1c35e763c4a5956fa0ae08c7cc41bb85b8877
patrick.lafrance <patrick.lafrance@outlook.com>
2019-10-18 13:21:45 -0400

    wip

--- storybook/config/config.js
+++ storybook/config/config.js
@@ -32,11 +32,32 @@ if (!isChromatic) {
 }
 
 import "./styles/components-presets.css";
+import "./styles/fix-docs-addon.css";
 import "./styles/stories.css";
 
 addParameters({
     options: {
-        theme: customStorybookTheme
+        theme: customStorybookTheme,
+        storySort: (a, b) => {
+            // console.log(a[1]);
+
+            if (a[1].kind === b[1].kind) {
+                if (a[1].parameters.displayName === "default") {
+                    return -1;
+                }
+                else if (b[1].parameters.displayName === "default") {
+                    return 1;
+                }
+                else if (a[1].parameters.displayName === "knobs") {
+                    return -1;
+                }
+                else if (b[1].parameters.displayName === "knobs") {
+                    return 1;
+                }
+            }
+
+            return a[1].id.localeCompare(b[1].id);
+        }
     },
     docs: {
         inlineStories: true
