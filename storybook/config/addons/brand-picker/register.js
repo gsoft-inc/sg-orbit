c68eb47484a44dfdf9a0b86e9104f07aa560b9a0
patrick.lafrance <patrick.lafrance@outlook.com>
2019-10-22 09:43:10 -0400

    wip

--- storybook/config/addons/brand-picker/register.js
+++ storybook/config/addons/brand-picker/register.js
@@ -3,6 +3,8 @@ import { isChromatic } from "../../env";
 import { renderTool } from "./brand-picker-tool";
 import addons, { types } from "@storybook/addons";
 
+// TODO: Test if the chromatic flag is now available in the manager ui
+
 addons.register(ADDON_ID, () => {
     if (!isChromatic) {
         addons.add(ADDON_ID, {
