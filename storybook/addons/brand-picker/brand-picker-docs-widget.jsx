073edc228877368b68223cccde198e6cc0ff6bd1
Alexandre Asselin <alexandre.asselin@gsoft.com>
2019-12-16 11:11:51 -0500

    Update the style if the currentBrand from the local storage is changed from a different tab.

--- storybook/addons/brand-picker/brand-picker-docs-widget.jsx
+++ storybook/addons/brand-picker/brand-picker-docs-widget.jsx
@@ -1,8 +1,10 @@
 import { BRANDS, useStorage } from "../../brands";
 import { COLORS_WEIGHT, getBrandColorVariableName, getPrimaryColorVariableName } from "./brands";
+import { useEffect, useRef } from "react";
 
 export function BrandPickerDocsWidget() {
     const [currentBrand, setCurrentBrand] = useStorage();
+    const previousBrand = useRef(currentBrand);
 
     const applyBrand = brand => {
         const computedStyle = window.getComputedStyle(document.documentElement);
@@ -16,6 +18,13 @@ export function BrandPickerDocsWidget() {
         applyBrand(brand);
         setCurrentBrand(brand);
     };
+
+    useEffect(() => {
+        if(previousBrand.current && previousBrand.current.id !== currentBrand.id) {
+            applyBrand(currentBrand);
+            previousBrand.current = currentBrand;
+        }
+    }, [currentBrand])
 
     return (
         <ul className="flex flex-row justify-end list pl0 mb7 mt8">
