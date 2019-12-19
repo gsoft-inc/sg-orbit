3c461d12680cc9e25c67ab1496e8778ce8143555
patrick.lafrance <patrick.lafrance@outlook.com>
2019-12-18 22:53:23 -0500

    Improved buttons docs

--- storybook/stories/components/warning/warning.jsx
+++ storybook/stories/components/warning/warning.jsx
@@ -1,12 +1,12 @@
 import styles from "./warning.module.css";
 
-import { InfoIcon24 } from "@orbit-ui/icons";
+import { InfoIcon } from "@orbit-ui/icons";
 import { Message } from "semantic-ui-react";
 
 export function Warning({ children }) {
     return (
         <Message warning size="small">
-            <span className={styles.icon}><InfoIcon24 /></span> {children}
+            <span className={styles.icon}><InfoIcon className="w6 h6" /></span> {children}
         </Message>
     );
 }
