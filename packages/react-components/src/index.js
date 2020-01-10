efc4bcb0671be5361a6e42c3ba020cd9f9fa2382
patrick.lafrance <patrick.lafrance@outlook.com>
2020-01-10 14:28:03 -0500

    Fix documentation for the buttons

--- packages/react-components/src/index.js
+++ packages/react-components/src/index.js
@@ -3,3 +3,4 @@ export * from "@orbit-ui/react-date-picker";
 export * from "@orbit-ui/react-popup";
 export * from "@orbit-ui/react-multi-select";
 export * from "@orbit-ui/react-search-input";
+export * from "@orbit-ui/react-buttons";
