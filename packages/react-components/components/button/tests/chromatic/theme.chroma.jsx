04e6b2db49b8b723368811275c350a784ba02ca6
Francis Thibault <francis.thibault@gsoft.com>
2020-01-24 08:38:46 -0500

    wip

--- packages/react-components/components/button/tests/chromatic/theme.chroma.jsx
+++ packages/react-components/components/button/tests/chromatic/theme.chroma.jsx
@@ -1,5 +1,6 @@
 import { Button } from "@orbit-ui/react-button/src";
 import { Buttons } from "./buttons";
+import { CalendarIcon24, ImageIcon, SignoutIcon24 } from "@orbit-ui/icons";
 import { createChromaticSection, paramsBuilder, storiesOfBuilder } from "@utils";
 import styles from "./styles.module.css";
 
@@ -71,14 +72,140 @@ stories("/theme")
              <div className="flex flex-row">
                  <div className="flex flex-column items-start">
                      <Button.Group>
-                         <Button disabled>One</Button>
-                         <Button active>Two</Button>
+                         <Button>One</Button>
+                         <Button>Two</Button>
                          <Button>Three</Button>
                      </Button.Group>
-                     <Button.Group basic>
+                     <Button.Group>
                          <Button disabled>One</Button>
                          <Button active>Two</Button>
+                     </Button.Group>
+                     <Button.Group negative>
+                         <Button>One</Button>
+                         <Button>Two</Button>
                          <Button>Three</Button>
+                     </Button.Group>
+                     <Button.Group negative>
+                         <Button disabled>One</Button>
+                         <Button active>Two</Button>
+                     </Button.Group>
+                     <Button.Group positive>
+                         <Button>One</Button>
+                         <Button>Two</Button>
+                         <Button>Three</Button>
+                     </Button.Group>
+                     <Button.Group positive>
+                         <Button disabled>One</Button>
+                         <Button active>Two</Button>
+                     </Button.Group>
+                     <Button.Group primary>
+                         <Button>One</Button>
+                         <Button>Two</Button>
+                         <Button>Three</Button>
+                     </Button.Group>
+                     <Button.Group primary>
+                         <Button disabled>One</Button>
+                         <Button active>Two</Button>
+                     </Button.Group>
+                     <Button.Group secondary>
+                         <Button>One</Button>
+                         <Button>Two</Button>
+                         <Button>Three</Button>
+                     </Button.Group>
+                     <Button.Group secondary>
+                         <Button disabled>One</Button>
+                         <Button active>Two</Button>
+                     </Button.Group>
+                     <Button.Group icon secondary>
+                         <Button><SignoutIcon24 /></Button>
+                         <Button><CalendarIcon24 /></Button>
+                         <Button><ImageIcon /></Button>
+                     </Button.Group>
+                     <Button.Group icon secondary>
+                         <Button disabled><SignoutIcon24 /></Button>
+                         <Button active><CalendarIcon24 /></Button>
+                     </Button.Group>
+                     <Button.Group icon basic>
+                         <Button><SignoutIcon24 /></Button>
+                         <Button><CalendarIcon24 /></Button>
+                         <Button><ImageIcon /></Button>
+                     </Button.Group>
+                 </div>
+                 <div className="flex flex-column items-start">
+                     <Button.Group compact>
+                         <Button>One</Button>
+                         <Button>Two</Button>
+                         <Button>Three</Button>
+                     </Button.Group>
+                     <Button.Group size="tiny">
+                         <Button>One</Button>
+                         <Button>Two</Button>
+                         <Button>Three</Button>
+                     </Button.Group>
+                     <Button.Group size="small">
+                         <Button>One</Button>
+                         <Button>Two</Button>
+                         <Button>Three</Button>
+                     </Button.Group>
+                     <Button.Group size="large">
+                         <Button>One</Button>
+                         <Button>Two</Button>
+                         <Button>Three</Button>
+                     </Button.Group>
+                     <Button.Group size="tiny" compact>
+                         <Button>One</Button>
+                         <Button>Two</Button>
+                         <Button>Three</Button>
+                     </Button.Group>
+                     <Button.Group size="small" compact>
+                         <Button>One</Button>
+                         <Button>Two</Button>
+                         <Button>Three</Button>
+                     </Button.Group>
+                     <Button.Group size="large" compact>
+                         <Button>One</Button>
+                         <Button>Two</Button>
+                         <Button>Three</Button>
+                     </Button.Group>
+                     <Button.Group size="tiny" fluid>
+                         <Button>One</Button>
+                         <Button>Two</Button>
+                         <Button>Three</Button>
+                     </Button.Group>
+                 </div>
+                 <div className="flex flex-column items-start">
+                     <Button.Group basic widths="3">
+                         <Button>One</Button>
+                         <Button>Two</Button>
+                         <Button>Cheese Pizza</Button>
+                     </Button.Group>
+                     <Button.Group toggle basic>
+                         <Button>One</Button>
+                         <Button>Two</Button>
+                         <Button>Cheese Pizza</Button>
+                     </Button.Group>
+                     <Button.Group basic widths="16">
+                         <Button>1</Button>
+                         <Button>2</Button>
+                         <Button>3</Button>
+                         <Button>4</Button>
+                         <Button>5</Button>
+                         <Button>6</Button>
+                         <Button>7</Button>
+                         <Button>8</Button>
+                         <Button>9</Button>
+                         <Button>10</Button>
+                         <Button>11</Button>
+                         <Button>12</Button>
+                         <Button>13</Button>
+                         <Button>14</Button>
+                         <Button>15</Button>
+                         <Button>16</Button>
+                     </Button.Group>
+                     <Button.Group basic vertical>
+                         <Button>One</Button>
+                         <Button>Two</Button>
+                         <Button>Cheese Pizza</Button>
                      </Button.Group>
                  </div>
              </div>
