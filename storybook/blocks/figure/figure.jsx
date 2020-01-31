e5639a5f000bde8d3acfb4b3f93a33df0ccab5d7
Francis Thibault <francis.thibault@gsoft.com>
2020-01-31 14:48:33 -0500

    font / figure supports link

--- storybook/blocks/figure/figure.jsx
+++ storybook/blocks/figure/figure.jsx
@@ -3,7 +3,9 @@ import styles from "./figure.module.css";
 export function Figure({ url, caption, width, height }) {
     return (
         <figure className={styles.figure}>
-            <img src={url} alt={caption} width={width} height={height} />
+            <a href={url} target="_blank" rel="noopener noreferrer" >
+                <img src={url} alt={caption} width={width} height={height} />
+            </a>
             <figcaption className="i">{ caption }</figcaption>
         </figure>
     );
