8af31e41e5c7f599a812a5fa42be31a1e7bf9b21
patrick.lafrance <patrick.lafrance@outlook.com>
2020-01-31 14:33:32 -0500

    Rework the Flexbox utilities table

---
+++ storybook/stories/materials/flexbox/stories/components/utilities-table/utilities-table.jsx
@@ -0,0 +1,32 @@
+import { Table } from "@blocks";
+import { arrayOf, element, oneOfType, shape, string } from "prop-types";
+
+import styles from "./utilities-table.module.css";
+
+const propTypes = {
+    utilities: arrayOf(shape({
+        name: string.isRequired,
+        description: string.isRequired,
+        example: element.isRequired
+    }))
+};
+
+function toRowValues({ name, description, example }) {
+    return [
+        <><span className="b">{name}</span><br /><span className="i">{description}</span></>,
+        example
+    ];
+}
+
+export function UtilitiesTable({ utilities }) {
+    return (
+        <Table 
+            columns={[
+                { title: "Class" },
+                { title: "", className: styles.example }
+            ]}
+            rows={utilities.map(x => toRowValues(x))}
+            rowClassName="h13"
+        />
+    )
+}
