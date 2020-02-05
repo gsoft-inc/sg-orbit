d99af98d521dc3f4ec52202824f00fb284facede
patrick.lafrance <patrick.lafrance@outlook.com>
2020-02-04 21:58:21 -0500

    Standardize size docs for components

--- packages/react-components/components/button/src/button.jsx
+++ packages/react-components/components/button/src/button.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/forbid-foreign-prop-types */
+
 import { ArgumentError, mergeClasses, throwWhenUnsupportedPropIsProvided } from "@orbit-ui/react-components-shared";
 import { Children, cloneElement, forwardRef } from "react";
 import { Ref, Button as SemanticButton } from "semantic-ui-react";
@@ -170,8 +172,6 @@ export const Button = forwardRef((props, ref) => (
     x.Group = SemanticButton.Group;
 });
 
-// eslint-disable-next-line react/forbid-foreign-prop-types
 if (!isNil(SemanticButton.propTypes)) {
-    // eslint-disable-next-line react/forbid-foreign-prop-types
     SemanticButton.propTypes.size = oneOf(["tiny", "small", "medium", "large"]);
 }
--- packages/react-components/components/label/src/label.jsx
+++ packages/react-components/components/label/src/label.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/forbid-foreign-prop-types */
+
 import { ArgumentError, mergeClasses, throwWhenUnsupportedPropIsProvided } from "@orbit-ui/react-components-shared";
 import { Children, cloneElement, forwardRef } from "react";
 import { Ref, Label as SemanticLabel } from "semantic-ui-react";
@@ -166,4 +168,8 @@ export const Label = forwardRef((props, ref) => (
     x.Detail = SemanticLabel.Detail;
     x.Group = SemanticLabel.Group;
 });
+
+if (!isNil(SemanticLabel.propTypes)) {
+    SemanticLabel.propTypes.size = oneOf(["tiny", "small", "medium", "large", "big", "huge", "massive"]);
+}
 
