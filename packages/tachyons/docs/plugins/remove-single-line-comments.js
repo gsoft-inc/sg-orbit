24fa01befcf23f255b374b5a913cf7f997807106
patrick.lafrance <patrick.lafrance@outlook.com>
2020-03-01 20:40:41 -0500

    Code cleanup

--- packages/tachyons/docs/plugins/remove-single-line-comments.js
+++ packages/tachyons/docs/plugins/remove-single-line-comments.js
@@ -4,17 +4,10 @@ module.exports = postcss.plugin("postcss-remove-single-line-comments", () => {
     return css => {
         css.walkComments(comment => {
             const { source } = comment;
-            // console.log(comment);
-            // console.log("*******************");
-            // console.log("*******************");
 
             if (source.start.line === source.end.line) {
                 comment.remove();
             }
-
-            // if (i.raws.inline) {
-            //     i.remove();
-            // }
         });
     };
 });
