93d58c5077e7986fc22b92f9db4b0d4c69cbd08f
patrick.lafrance <patrick.lafrance@outlook.com>
2020-03-27 16:19:30 -0400

    Fix storybook config

--- packages/react-components/src/message/tests/chromatic/message.chroma.jsx
+++ packages/react-components/src/message/tests/chromatic/message.chroma.jsx
@@ -1,6 +1,6 @@
 import { Button } from "@react-components/button";
 import { GroupIcon } from "@react-components/icons";
-import { Message } from "@react-components/Message";
+import { Message } from "@react-components/message";
 import { createChromaticSection, paramsBuilder, storiesOfBuilder } from "@utils";
 
 function stories(segment) {
