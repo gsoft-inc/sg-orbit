ebf9d1f995a50d3eeea60e116e1ef317d92ec122
patrick.lafrance <patrick.lafrance@outlook.com>
2020-05-15 10:13:33 -0400

    removed inline prop from avatar element

--- packages/react-components/src/select/src/monkey-patch-semantic-dropdown.jsx
+++ packages/react-components/src/select/src/monkey-patch-semantic-dropdown.jsx
@@ -14,7 +14,8 @@ export class MonkeyPatchSemanticDropdown extends Dropdown {
 
         return (
             <>
-                {renderAvatar(avatar, !inline ? size : undefined, { inline })}
+                {/* {renderAvatar(avatar, !inline ? size : undefined, { inline })} */}
+                {renderAvatar(avatar, !inline ? size : undefined)}
                 {text}
             </>
         );
