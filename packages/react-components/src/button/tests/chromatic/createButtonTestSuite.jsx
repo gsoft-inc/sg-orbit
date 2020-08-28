5ac6ffc4e6d47ee044641d4613aa09b80f9e67a3
patricklafrance <pa.lafrance@gmail.com>
2020-08-28 10:57:32 -0400

    Remove mini & tiny size from button tests

--- packages/react-components/src/button/tests/chromatic/createButtonTestSuite.jsx
+++ packages/react-components/src/button/tests/chromatic/createButtonTestSuite.jsx
@@ -12,15 +12,11 @@ export function createButtonTestSuite(element, stories) {
         .add("default", () =>
             <Stack>
                 <Inline align="end">
-                    <Button size="mini" element={element}>Button</Button>
-                    <Button size="tiny" element={element}>Button</Button>
                     <Button size="small" element={element}>Button</Button>
                     <Button element={element}>Button</Button>
                     <Button size="large" element={element}>Button</Button>
                 </Inline>
                 <Inline align="end">
-                    <Button loading size="mini" element={element}>Button</Button>
-                    <Button loading size="tiny" element={element}>Button</Button>
                     <Button loading size="small" element={element}>Button</Button>
                     <Button loading element={element}>Button</Button>
                     <Button loading size="large" element={element}>Button</Button>
@@ -39,22 +35,16 @@ export function createButtonTestSuite(element, stories) {
         .add("icon", () =>
             <Stack>
                 <Inline align="end">
-                    <Button icon={<SignoutIcon />} size="mini" element={element}>Button</Button>
-                    <Button icon={<SignoutIcon />} size="tiny" element={element}>Button</Button>
                     <Button icon={<SignoutIcon />} size="small" element={element}>Button</Button>
                     <Button icon={<SignoutIcon />} element={element}>Button</Button>
                     <Button icon={<SignoutIcon />} size="large" element={element}>Button</Button>
                 </Inline>
                 <Inline align="end">
-                    <Button icon={<IconGroup><SignoutIcon /><SignoutIcon /><SignoutIcon /></IconGroup>} size="mini" element={element}>Button</Button>
-                    <Button icon={<IconGroup><SignoutIcon /><SignoutIcon /><SignoutIcon /></IconGroup>} size="tiny" element={element}>Button</Button>
                     <Button icon={<IconGroup><SignoutIcon /><SignoutIcon /><SignoutIcon /></IconGroup>} size="small" element={element}>Button</Button>
                     <Button icon={<IconGroup><SignoutIcon /><SignoutIcon /><SignoutIcon /></IconGroup>} element={element}>Button</Button>
                     <Button icon={<IconGroup><SignoutIcon /><SignoutIcon /><SignoutIcon /></IconGroup>} size="large" element={element}>Button</Button>
                 </Inline>
                 <Inline align="end">
-                    <Button loading icon={<SignoutIcon />} size="mini" element={element}>Button</Button>
-                    <Button loading icon={<SignoutIcon />} size="tiny" element={element}>Button</Button>
                     <Button loading icon={<SignoutIcon />} size="small" element={element}>Button</Button>
                     <Button loading icon={<SignoutIcon />} element={element}>Button</Button>
                     <Button loading icon={<SignoutIcon />} size="large" element={element}>Button</Button>
@@ -70,15 +60,11 @@ export function createButtonTestSuite(element, stories) {
         .add("counter", () =>
             <Stack>
                 <Inline align="end">
-                    <Button counter={<Counter>60</Counter>} size="mini" element={element}>Button</Button>
-                    <Button counter={<Counter>60</Counter>} size="tiny" element={element}>Button</Button>
                     <Button counter={<Counter>60</Counter>} size="small" element={element}>Button</Button>
                     <Button counter={<Counter>60</Counter>} element={element}>Button</Button>
                     <Button counter={<Counter>60</Counter>} size="large" element={element}>Button</Button>
                 </Inline>
                 <Inline align="end">
-                    <Button loading counter={<Counter>60</Counter>} size="mini" element={element}>Button</Button>
-                    <Button loading counter={<Counter>60</Counter>} size="tiny" element={element}>Button</Button>
                     <Button loading counter={<Counter>60</Counter>} size="small" element={element}>Button</Button>
                     <Button loading counter={<Counter>60</Counter>} element={element}>Button</Button>
                     <Button loading counter={<Counter>60</Counter>} size="large" element={element}>Button</Button>
@@ -94,8 +80,6 @@ export function createButtonTestSuite(element, stories) {
         .add("primary", () =>
             <Stack>
                 <Inline align="end">
-                    <Button color="primary" size="mini" element={element}>Button</Button>
-                    <Button color="primary" size="tiny" element={element}>Button</Button>
                     <Button color="primary" size="small" element={element}>Button</Button>
                     <Button color="primary" element={element}>Button</Button>
                     <Button color="primary" size="large" element={element}>Button</Button>
@@ -109,8 +93,6 @@ export function createButtonTestSuite(element, stories) {
         .add("secondary", () =>
             <Stack>
                 <Inline align="end">
-                    <Button color="secondary" size="mini" element={element}>Button</Button>
-                    <Button color="secondary" size="tiny" element={element}>Button</Button>
                     <Button color="secondary" size="small" element={element}>Button</Button>
                     <Button color="secondary" element={element}>Button</Button>
                     <Button color="secondary" size="large" element={element}>Button</Button>
@@ -124,8 +106,6 @@ export function createButtonTestSuite(element, stories) {
         .add("danger", () =>
             <Stack>
                 <Inline align="end">
-                    <Button color="danger" size="mini" element={element}>Button</Button>
-                    <Button color="danger" size="tiny" element={element}>Button</Button>
                     <Button color="danger" size="small" element={element}>Button</Button>
                     <Button color="danger" element={element}>Button</Button>
                     <Button color="danger" size="large" element={element}>Button</Button>
@@ -139,29 +119,21 @@ export function createButtonTestSuite(element, stories) {
         .add("circular", () =>
             <Stack>
                 <Inline align="end">
-                    <Button shape="circular" size="mini" element={element}>Aa</Button>
-                    <Button shape="circular" size="tiny" element={element}>Aa</Button>
                     <Button shape="circular" size="small" element={element}>Aa</Button>
                     <Button shape="circular" element={element}>Aa</Button>
                     <Button shape="circular" size="large" element={element}>Aa</Button>
                 </Inline>
                 <Inline align="end">
-                    <Button loading shape="circular" size="mini" element={element}>Aa</Button>
-                    <Button loading shape="circular" size="tiny" element={element}>Aa</Button>
                     <Button loading shape="circular" size="small" element={element}>Aa</Button>
                     <Button loading shape="circular" element={element}>Aa</Button>
                     <Button loading shape="circular" size="large" element={element}>Aa</Button>
                 </Inline>
                 <Inline align="end">
-                    <Button shape="circular" size="mini" element={element}><SignoutIcon /></Button>
-                    <Button shape="circular" size="tiny" element={element}><SignoutIcon /></Button>
                     <Button shape="circular" size="small" element={element}><SignoutIcon /></Button>
                     <Button shape="circular" element={element}><SignoutIcon /></Button>
                     <Button shape="circular" size="large" element={element}><SignoutIcon /></Button>
                 </Inline>
                 <Inline align="end">
-                    <Button loading shape="circular" size="mini" element={element}><SignoutIcon /></Button>
-                    <Button loading shape="circular" size="tiny" element={element}><SignoutIcon /></Button>
                     <Button loading shape="circular" size="small" element={element}><SignoutIcon /></Button>
                     <Button loading shape="circular" element={element}><SignoutIcon /></Button>
                     <Button loading shape="circular" size="large" element={element}><SignoutIcon /></Button>
@@ -171,15 +143,11 @@ export function createButtonTestSuite(element, stories) {
         .add("rounded", () =>
             <Stack>
                 <Inline align="end">
-                    <Button shape="rounded" size="mini" element={element}>Aa</Button>
-                    <Button shape="rounded" size="tiny" element={element}>Aa</Button>
                     <Button shape="rounded" size="small" element={element}>Aa</Button>
                     <Button shape="rounded" element={element}>Aa</Button>
                     <Button shape="rounded" size="large" element={element}>Aa</Button>
                 </Inline>
                 <Inline align="end">
-                    <Button loading shape="rounded" size="mini" element={element}>Aa</Button>
-                    <Button loading shape="rounded" size="tiny" element={element}>Aa</Button>
                     <Button loading shape="rounded" size="small" element={element}>Aa</Button>
                     <Button loading shape="rounded" element={element}>Aa</Button>
                     <Button loading shape="rounded" size="large" element={element}>Aa</Button>
@@ -189,40 +157,30 @@ export function createButtonTestSuite(element, stories) {
         .add("states", () =>
             <Stack>
                 <Inline align="end">
-                    <Button active size="mini" element={element}>Button</Button>
-                    <Button active size="tiny" element={element}>Button</Button>
                     <Button active size="small" element={element}>Button</Button>
                     <Button active element={element}>Button</Button>
                     <Button active size="large" element={element}>Button</Button>
                     <Button loading active element={element}>Button</Button>
                 </Inline>
                 <Inline align="end">
-                    <Button focus size="mini" element={element}>Button</Button>
-                    <Button focus size="tiny" element={element}>Button</Button>
                     <Button focus size="small" element={element}>Button</Button>
                     <Button focus element={element}>Button</Button>
                     <Button focus size="large" element={element}>Button</Button>
                     <Button loading focus element={element}>Button</Button>
                 </Inline>
                 <Inline align="end">
-                    <Button hover size="mini" element={element}>Button</Button>
-                    <Button hover size="tiny" element={element}>Button</Button>
                     <Button hover size="small" element={element}>Button</Button>
                     <Button hover element={element}>Button</Button>
                     <Button hover size="large" element={element}>Button</Button>
                     <Button loading hover element={element}>Button</Button>
                 </Inline>
                 <Inline align="end">
-                    <Button focus hover size="mini" element={element}>Button</Button>
-                    <Button focus hover size="tiny" element={element}>Button</Button>
                     <Button focus hover size="small" element={element}>Button</Button>
                     <Button focus hover element={element}>Button</Button>
                     <Button focus hover size="large" element={element}>Button</Button>
                     <Button loading focus hover element={element}>Button</Button>
                 </Inline>
                 <Inline align="end">
-                    <Button disabled size="mini" element={element}>Button</Button>
-                    <Button disabled size="tiny" element={element}>Button</Button>
                     <Button disabled size="small" element={element}>Button</Button>
                     <Button disabled element={element}>Button</Button>
                     <Button disabled size="large" element={element}>Button</Button>
