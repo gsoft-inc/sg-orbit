7bddee7f5141b661352ccfb125278be4cec4904a
patricklafrance <pa.lafrance@gmail.com>
2020-09-29 10:47:51 -0400

    Updated link docs

--- packages/react-components/src/link/docs/link/Card.jsx
+++ packages/react-components/src/link/docs/link/Card.jsx
@@ -1,6 +1,6 @@
 export function Card({ children }) {
     return (
-        <div className="flex flex-column items-center pa5 ba b--cloud-50 br3 w-40">
+        <div className="flex flex-column items-center pa5 ba b--cloud-50 br3">
             {children}
         </div>
     );
