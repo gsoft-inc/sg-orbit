91680ff9bb7d288cb63bba2235d216728263ed60
Francis Thibault <francis.thibault@gsoft.com>
2020-09-30 14:48:47 -0400

    border radius passed to child

--- packages/react-components/src/link/docs/link/Card.jsx
+++ packages/react-components/src/link/docs/link/Card.jsx
@@ -1,6 +1,6 @@
 export function Card({ children }) {
     return (
-        <div className="flex flex-column items-center pa5 ba b--cloud-50" style={{ borderRadius: "var(--o-ui-rounded-border-radius)" }}>
+        <div className="flex flex-column items-center pa5 ba b--cloud-50">
             {children}
         </div>
     );
