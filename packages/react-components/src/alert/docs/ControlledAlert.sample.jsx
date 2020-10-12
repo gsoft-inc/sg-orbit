93141abd4af8cd4461df0f749147a06a2b7a00ac
patricklafrance <pa.lafrance@gmail.com>
2020-10-12 17:30:58 -0400

    Additional Alert components docs

---
+++ packages/react-components/src/alert/docs/ControlledAlert.sample.jsx
@@ -0,0 +1,29 @@
+import { Alert } from "@react-components/alert";
+import { Button } from "@react-components/button";
+import { Content } from "@react-components/view";
+import { EmailIcon } from "@react-components/icons";
+import { Stack } from "@react-components/layout";
+import { useCallback, useState } from "react";
+
+export function ControlledAlert() {
+    const [show, setShow] = useState(true);
+
+    const toggleAlert = useCallback(() => {
+        setShow(x => !x);
+        console.log(!show);
+    }, [show, setShow]);
+
+    return (
+        <Stack>
+            <Button size="sm" color="secondary" onClick={toggleAlert}>{show ? "Hide" : "Show"}</Button>
+            <Alert
+                show={show}
+                onDismiss={toggleAlert}
+            >
+                <EmailIcon />
+                <Content>Your email to <strong>booking@spacetravel.com</strong> has been sent!</Content>
+                <Button>Undo</Button>
+            </Alert>
+        </Stack>
+    );
+}
