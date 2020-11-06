7058a5c9970e86ae1d603e926d2b125dfb23b54a
patricklafrance <pa.lafrance@gmail.com>
2020-11-06 17:15:31 -0500

    Added ids tests to Tabs

---
+++ packages/react-components/src/disclosure/tests/jest/Disclosure.test.jsx
@@ -0,0 +1,6 @@
+import { Button } from "@react-components/button";
+import { Content } from "@react-components/view";
+import { Disclosure } from "@react-components/disclosure";
+import { KEYS } from "@react-components/shared";
+import { act, fireEvent, render, waitFor } from "@testing-library/react";
+import { createRef } from "react";
