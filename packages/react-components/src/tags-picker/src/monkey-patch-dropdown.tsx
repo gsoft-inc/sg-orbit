100ad4dc443dcc27b5166785b3deaa66bc3ec5b7
Alexandre Asselin <alexandre.asselin@gsoft.com>
2021-02-02 14:18:50 -0500

    converted the only file that cause an issue with type generation

--- packages/react-components/src/tags-picker/src/monkey-patch-dropdown.tsx
+++ packages/react-components/src/tags-picker/src/monkey-patch-dropdown.tsx
@@ -7,8 +7,8 @@ import keyboardKey from "keyboard-key";
 // https://github.com/Semantic-Org/Semantic-UI-React/issues/3768
 // Sadly, specifying "search" doesn't work for us since it renders the dropdown as a "combolist" instead of a "listbox" and it cause a
 // whole new set of problems.
-export class MonkeyPatchDropdown extends Dropdown {
-    selectItemOnEnter = e => {
+class MonkeyPatchDropdown extends Dropdown {
+    selectItemOnEnter = (e: any) => {
         const { search } = this.props;
 
         if (keyboardKey.getCode(e) !== keyboardKey.Enter) {
@@ -17,18 +17,20 @@ export class MonkeyPatchDropdown extends Dropdown {
 
         e.preventDefault();
 
-        const optionSize = size(this.getMenuOptions());
+        const optionSize = size((this as any).getMenuOptions());
 
         if (search && optionSize === 0) {
             return;
         }
 
-        this.makeSelectedItemActive(e);
-        this.closeOnChange(e);
-        this.clearSearchQuery();
+        (this as any).makeSelectedItemActive(e);
+        (this as any).closeOnChange(e);
+        (this as any).clearSearchQuery();
 
         if (search) {
-            invoke(this.searchRef.current, "focus");
+            invoke((this as any).searchRef.current, "focus");
         }
     }
 }
+
+export { MonkeyPatchDropdown };
