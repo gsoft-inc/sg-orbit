f8fe8f9b2fb8aa44507978a7cc5e7ea66480eea5
Alexandre Asselin <alexandre.asselin@gsoft.com>
2021-02-04 17:06:10 -0500

    conversion de keyboard navigation

---
+++ packages/react-components/src/shared/src/useKeyboardNavigation.ts
@@ -0,0 +1,45 @@
+import { isNil } from "lodash";
+import { KeyboardEventHandler } from "react";
+import { useEventCallback } from "./useEventCallback";
+import type { FocusElementOptions, FocusManager } from "./useFocusManager"
+
+interface KeyboardNavigationOptions {
+    onSelect?(event: KeyboardEvent, element: Element): void;
+}
+
+interface KeyboardNavigationBindings {
+    previous: string[];
+    next: string[];
+    first: string[];
+    last: string[];
+}
+
+export function useKeyboardNavigation(focusManager: FocusManager, { previous = [], next = [], first = [], last = [] }: KeyboardNavigationBindings, { onSelect }: KeyboardNavigationOptions = {}) {
+    const handleKeyDown: KeyboardEventHandler = useEventCallback((event: KeyboardEvent) => {
+        const keyCode = event.keyCode;
+
+        const handleFocus: FocusElementOptions["onFocus"] = element => {
+            if (!isNil(onSelect)) {
+                onSelect(event, element);
+            }
+        };
+
+        if (previous.includes(keyCode.toString())) {
+            event.preventDefault();
+            focusManager.focusPrevious({ onFocus: handleFocus });
+        } else if (next.includes(keyCode.toString())) {
+            event.preventDefault();
+            focusManager.focusNext({ onFocus: handleFocus });
+        } else if (first.includes(keyCode.toString())) {
+            event.preventDefault();
+            focusManager.focusFirst({ onFocus: handleFocus });
+        } else if (last.includes(keyCode.toString())) {
+            event.preventDefault();
+            focusManager.focusLast({ onFocus: handleFocus });
+        }
+    });
+
+    return {
+        onKeyDown: handleKeyDown
+    };
+}
