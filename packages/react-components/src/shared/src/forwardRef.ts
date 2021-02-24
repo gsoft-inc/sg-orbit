ff77ae19d439d2221c54c4f80a89f1e4ee801e03
Alexandre Asselin <alexandre.asselin@gsoft.com>
2021-02-24 15:22:12 -0500

    review fixes

--- packages/react-components/src/shared/src/forwardRef.ts
+++ packages/react-components/src/shared/src/forwardRef.ts
@@ -2,7 +2,7 @@
 import { ComponentProps, ElementRef, ElementType, ForwardRefExoticComponent, ForwardRefRenderFunction, HTMLProps, RefAttributes, forwardRef as reactForwardRef } from "react";
 
 type PropsOf<T> =
-    T extends ElementType ? ComponentProps<T> & RefAttributes<ElementRef<T>> :
+    T extends ElementType ? HTMLProps<T> & ComponentProps<T> & RefAttributes<ElementRef<T>> :
     T extends HTMLElement ? HTMLProps<T> & RefAttributes<T> :
     never;
 
@@ -20,10 +20,10 @@ type MergeWithAs<T, P> = RightJoinProps<PropsOf<T>, OmitCommonProps<P>> & {
     [key: string]: any;
 };
 
-export type ForwardedOrbitComponent<T, P extends Record<string, any>> = ForwardRefExoticComponent<MergeWithAs<T, P>>
+export type OrbitComponent<T, P extends Record<string, any>> = ForwardRefExoticComponent<MergeWithAs<T, P>>
 
 export type AsAttribute = { as?: ElementType };
 
-export function forwardRef<P extends AsAttribute, T = HTMLElement>(render: ForwardRefRenderFunction<T, P>): ForwardedOrbitComponent<T, P> {
-    return (reactForwardRef(render) as unknown) as ForwardedOrbitComponent<T, P>;
+export function forwardRef<P extends AsAttribute, T = HTMLElement>(render: ForwardRefRenderFunction<T, P>): OrbitComponent<T, P> {
+    return (reactForwardRef(render) as unknown) as OrbitComponent<T, P>;
 }
