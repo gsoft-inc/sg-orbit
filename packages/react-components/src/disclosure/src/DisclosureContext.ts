0195f2d014f2b08e089d465e4b96538de2cce7b1
Alexandre Asselin <alexandre.asselin@gsoft.com>
2021-02-26 08:14:33 -0500

    forgot typings

--- packages/react-components/src/disclosure/src/DisclosureContext.ts
+++ packages/react-components/src/disclosure/src/DisclosureContext.ts
@@ -1,7 +1,11 @@
 import { createContext, useContext } from "react";
 
-export const DisclosureContext = createContext({});
+export interface DisclosureContextProps {
+    isOpen?: boolean;
+}
 
-export function useDisclosureContext() {
+export const DisclosureContext = createContext<DisclosureContextProps>({});
+
+export function useDisclosureContext(): DisclosureContextProps {
     return useContext(DisclosureContext);
 }
