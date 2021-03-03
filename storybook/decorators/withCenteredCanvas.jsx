0082d3d77c6835cf598cddf09725afaf9bc1e6e4
patricklafrance <pa.lafrance@gmail.com>
2021-03-03 13:33:08 -0500

    Try to fix chromatic tests height issue

--- storybook/decorators/withCenteredCanvas.jsx
+++ storybook/decorators/withCenteredCanvas.jsx
@@ -14,6 +14,7 @@ export function withCenteredCanvas(Story, context) {
                 className="center"
                 style={{
                     width: "90%",
+                    // Important for chromatic tests.
                     minHeight: "600px",
                     ...canvasLayout
                 }}
