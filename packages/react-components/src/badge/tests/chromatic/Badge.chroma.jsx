ecd75e38c1c827a70123f3c2a300fb2d23ed8c85
Francis Thibault <francis.thibault@gsoft.com>
2021-04-16 11:59:17 -0400

    mailicon was not imported correctly at some places

--- packages/react-components/src/badge/tests/chromatic/Badge.chroma.jsx
+++ packages/react-components/src/badge/tests/chromatic/Badge.chroma.jsx
@@ -1,5 +1,5 @@
 import { Badge } from "@react-components/badge";
-import { CheckCircleIcon, MailIcon } from "@react-components/icons";
+import { CheckCircleIcon, EmailIcon } from "@react-components/icons";
 import { Inline } from "@react-components/layout";
 import { Text } from "@react-components/text";
 import { storiesOfBuilder } from "@stories/utils";
@@ -38,7 +38,7 @@ function IconBadge({ children, ...rest }) {
             overlap="icon"
         >
             {children}
-            <MailIcon size="lg" />
+            <EmailIcon size="lg" />
         </Badge>
     );
 }
