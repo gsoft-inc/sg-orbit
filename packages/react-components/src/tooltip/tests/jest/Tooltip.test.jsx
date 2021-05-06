50364abcfdb9f591f91e204bd8692375f8e9baad
patricklafrance <pa.lafrance@gmail.com>
2021-05-06 11:02:01 -0400

    Added jest for tests Tooltip & TooltipTrigger

--- packages/react-components/src/tooltip/tests/jest/Tooltip.test.jsx
+++ packages/react-components/src/tooltip/tests/jest/Tooltip.test.jsx
@@ -1,7 +1,57 @@
-/*
-Aria:
-- a tooltip have the \"tooltip\" role
+import { Tooltip } from "@react-components/tooltip";
+import { createRef } from "react";
+import { render, waitFor } from "@testing-library/react";
 
-Refs:
-- classic refs test
-*/
+// ***** Aria *****
+
+test("a tooltip have the \"tooltip\" role", async () => {
+    const { getByTestId } = render(
+        <Tooltip data-testid="tooltip">Tooltip</Tooltip>
+    );
+
+    await waitFor(() => expect(getByTestId("tooltip")).toHaveAttribute("role", "tooltip"));
+});
+
+// ***** Refs *****
+
+test("ref is a DOM element", async () => {
+    const ref = createRef();
+
+    render(
+        <Tooltip ref={ref}>Tooltip</Tooltip>
+    );
+
+    await waitFor(() => expect(ref.current).not.toBeNull());
+
+    expect(ref.current instanceof HTMLElement).toBeTruthy();
+    expect(ref.current.tagName).toBe("DIV");
+});
+
+test("when using a callback ref, ref is a DOM element", async () => {
+    let refNode = null;
+
+    render(
+        <Tooltip
+            ref={node => {
+                refNode = node;
+            }}
+        >
+            Tooltip
+        </Tooltip>
+    );
+
+    await waitFor(() => expect(refNode).not.toBeNull());
+
+    expect(refNode instanceof HTMLElement).toBeTruthy();
+    expect(refNode.tagName).toBe("DIV");
+});
+
+test("set ref once", async () => {
+    const handler = jest.fn();
+
+    render(
+        <Tooltip ref={handler}>Tooltip</Tooltip>
+    );
+
+    await waitFor(() => expect(handler).toHaveBeenCalledTimes(1));
+});
