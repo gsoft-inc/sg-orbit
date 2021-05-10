cf7dba00b9c724490494ed561ff9f0925edb65ea
patricklafrance <pa.lafrance@gmail.com>
2021-05-10 18:05:50 -0400

    Added jest tests for DateRangeInput

--- packages/react-components/src/date-input/tests/jest/DateInput.test.jsx
+++ packages/react-components/src/date-input/tests/jest/DateInput.test.jsx
@@ -14,7 +14,7 @@ function type(element, text) {
     });
 }
 
-function backspace(element, times) {
+function backspace(element, times = 1) {
     for (let x = 0; x < times; x += 1) {
         act(() => {
             userEvent.type(element, "{backspace}");
@@ -46,7 +46,7 @@ test("only accept number characters", async () => {
     await waitFor(() => expect(getByTestId("date")).toHaveValue("01/0"));
 });
 
-test("when the input has no value and an incomplete date is typed, reset to an empty value on blur", async () => {
+test("when the input has no value and a partial date has been entered, reset to an empty value on blur", async () => {
     const { getByTestId } = render(
         <DateInput data-testid="date" />
     );
@@ -66,7 +66,7 @@ test("when the input has no value and an incomplete date is typed, reset to an e
     await waitFor(() => expect(getByTestId("date")).toHaveValue(""));
 });
 
-test("when the input has no value and an invalid date is typed, clear the input value", async () => {
+test("when the input has no value and an invalid date has been entered, clear the input value", async () => {
     const { getByTestId } = render(
         <DateInput data-testid="date" />
     );
@@ -80,7 +80,7 @@ test("when the input has no value and an invalid date is typed, clear the input 
     await waitFor(() => expect(getByTestId("date")).toHaveValue(""));
 });
 
-test("when the typed date is lower than the min date, reset value to min date", async () => {
+test("when the entered date is lower than the min date, reset value to min date", async () => {
     const { getByTestId } = render(
         <DateInput
             minDate={new Date(2021, 0, 1)}
@@ -97,7 +97,7 @@ test("when the typed date is lower than the min date, reset value to min date", 
     await waitFor(() => expect(getByTestId("date")).toHaveValue("01/01/2021"));
 });
 
-test("when the typed date is greater than the max date, reset the date to the max date value", async () => {
+test("when the entered date is greater than the max date, reset the date to the max date value", async () => {
     const { getByTestId } = render(
         <DateInput
             maxDate={new Date(2021, 0, 1)}
@@ -114,7 +114,7 @@ test("when the typed date is greater than the max date, reset the date to the ma
     await waitFor(() => expect(getByTestId("date")).toHaveValue("01/01/2021"));
 });
 
-test("when a complete date is typed, convert the date to a read format on blur", async () => {
+test("when a valid date has been entered, convert the date format to a read format on blur", async () => {
     const { getByTestId } = render(
         <DateInput data-testid="date" />
     );
@@ -132,7 +132,7 @@ test("when a complete date is typed, convert the date to a read format on blur",
     await waitFor(() => expect(getByTestId("date")).toHaveValue("Fri, Jan 1, 2021"));
 });
 
-test("when the input value has a valid date and receive focus, convert the date to an editable format", async () => {
+test("when the input value has a valid date and receive focus, convert the date format to an editable format", async () => {
     const { getByTestId } = render(
         <DateInput
             defaultValue={new Date(2021, 0, 1)}
@@ -153,7 +153,7 @@ test("when the input value has a valid date and receive focus, convert the date 
     await waitFor(() => expect(getByTestId("date")).toHaveValue("Fri, Jan 1, 2021"));
 });
 
-test("when the input value has a valid date and an incomplete date is typed, reset to the last valid date on blur", async () => {
+test("when the input value has a valid date and a partial date has been entered entered, reset to the last valid date on blur", async () => {
     const { getByTestId } = render(
         <DateInput
             defaultValue={new Date(2021, 0, 1)}
@@ -174,7 +174,7 @@ test("when the input value has a valid date and an incomplete date is typed, res
     await waitFor(() => expect(getByTestId("date")).toHaveValue("Fri, Jan 1, 2021"));
 });
 
-test("when the input value has a valid date and an invalid date is typed, reset to the last valid date", async () => {
+test("when the input value has a valid date and an invalid date has been entered, reset to the last valid date", async () => {
     const { getByTestId } = render(
         <DateInput
             defaultValue={new Date(2021, 0, 1)}
@@ -193,7 +193,7 @@ test("when the input value has a valid date and an invalid date is typed, reset 
     await waitFor(() => expect(getByTestId("date")).toHaveValue("01/01/2021"));
 });
 
-test("when in a field, clicking on the field label focus the autocomplete", async () => {
+test("when in a field, clicking on the field label focus the date input", async () => {
     const { getByTestId } = render(
         <Field>
             <Label data-testid="label">Date</Label>
@@ -210,7 +210,7 @@ test("when in a field, clicking on the field label focus the autocomplete", asyn
 
 // ***** Api *****
 
-test("when the input has no value and a complete date is typed, call onDateChange with the new date", async () => {
+test("when the input has no value and a valid date has been entered, call onDateChange with the new date", async () => {
     const handler = jest.fn();
 
     const { getByTestId } = render(
@@ -230,7 +230,7 @@ test("when the input has no value and a complete date is typed, call onDateChang
     await waitFor(() => expect(handler).toHaveBeenCalledWith(expect.anything(), new Date(2021, 0, 1)));
 });
 
-test("when the input has no value and an incomplete date is cleared, do not call onDateChange", async () => {
+test("when the input has no value and a partial date has been cleared, do not call onDateChange", async () => {
     const handler = jest.fn();
 
     const { getByTestId } = render(
@@ -257,7 +257,7 @@ test("when the input has no value and an incomplete date is cleared, do not call
     await waitFor(() => expect(handler).not.toHaveBeenCalled());
 });
 
-test("when the input has no value and an invalid date is typed, do not call onDateChange", async () => {
+test("when the input has no value and an invalid date has been entered, do not call onDateChange", async () => {
     const handler = jest.fn();
 
     const { getByTestId } = render(
@@ -278,7 +278,7 @@ test("when the input has no value and an invalid date is typed, do not call onDa
     await waitFor(() => expect(handler).not.toHaveBeenCalled());
 });
 
-test("when the input value has a valid date and a new valid date is typed, call onDateChange with the new date", async () => {
+test("when the input value has a valid date and a new valid date has been entered, call onDateChange with the new date", async () => {
     const handler = jest.fn();
 
     const { getByTestId } = render(
@@ -293,9 +293,7 @@ test("when the input value has a valid date and a new valid date is typed, call 
         getByTestId("date").focus();
     });
 
-    act(() => {
-        userEvent.type(getByTestId("date"), "{backspace}");
-    });
+    backspace(getByTestId("date"));
 
     act(() => {
         userEvent.type(getByTestId("date"), "0");
@@ -309,7 +307,7 @@ test("when the input value has a valid date and a new valid date is typed, call 
     await waitFor(() => expect(handler).toHaveBeenCalledWith(expect.anything(), new Date(2020, 0, 1)));
 });
 
-test("when the input value has a valid date and the date is cleared, call onDateChange with null", async () => {
+test("when the input value has a valid date and the date has been cleared, call onDateChange with null", async () => {
     const handler = jest.fn();
 
     const { getByTestId } = render(
@@ -332,7 +330,7 @@ test("when the input value has a valid date and the date is cleared, call onDate
     await waitFor(() => expect(handler).toHaveBeenCalledWith(expect.anything(), null));
 });
 
-test("when the input value has a valid date and an incomplete date is typed, do not call onDateChange on date reset", async () => {
+test("when the input value has a valid date and a partial date has been entered, do not call onDateChange on date reset", async () => {
     const handler = jest.fn();
 
     const { getByTestId } = render(
@@ -358,7 +356,7 @@ test("when the input value has a valid date and an incomplete date is typed, do 
     await waitFor(() => expect(handler).not.toHaveBeenCalled());
 });
 
-test("when the input value has a valid date and an invalid date is typed, do not call onDateChange", async () => {
+test("when the input value has a valid date and an invalid date has been entered, do not call onDateChange", async () => {
     const handler = jest.fn();
 
     const { getByTestId } = render(
