1f8448797e865c47eed48da4cffb8ed41908b547
Francis Thibault <francis.thibault@gsoft.com>
2021-06-15 14:56:54 -0400

    documentation fixes

--- docs/materials/motion/MotionSample.jsx
+++ docs/materials/motion/MotionSample.jsx
@@ -6,7 +6,7 @@ export function MotionSample({ duration, easing }) {
     const formattedDuration = `.${duration}s`;
     const formattedEasing = easing ? `cubic-bezier${easing}` : "cubic-bezier(0.22, 0.61, 0.36, 1)";
 
-    const [isAnimated, setAnimated] = useState("false");
+    const [isAnimated, setAnimated] = useState(false);
 
     const handleClick = () => {
         setAnimated(!isAnimated);
@@ -14,8 +14,9 @@ export function MotionSample({ duration, easing }) {
 
 
     return (
-        <div>
-            <div type="button" onClick={handleClick} className={`br-100 bn bg-marine-300 w10 h10 motion-sample ${isAnimated ? "slide" : null}`} style={{ "--duration": formattedDuration, "--easing": formattedEasing }}></div>
+        <div className="animation-player">
+            <button type="button" onClick={handleClick} className="play-animation" style={{ "--duration": formattedDuration, "--easing": formattedEasing }}>Play this motion</button>
+            <div className={`br-100 bn bg-marine-300 w10 h10 motion-sample ${isAnimated ? "slide" : null}`} style={{ "--duration": formattedDuration, "--easing": formattedEasing }}></div>
         </div>
     );
 }
