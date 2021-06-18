ee990d663570fc86f2f76771de81e3808dd1eea7
Francis Thibault <francis.thibault@gsoft.com>
2021-06-18 10:59:53 -0400

    retina doc for images

--- packages/react-components/src/image/docs/assets/index.js
+++ packages/react-components/src/image/docs/assets/index.js
@@ -1,3 +1,5 @@
-import LaunchImage from "./launch.jpg";
+import LaunchImage from "./launch.png";
+import LaunchImage2x from "./launch@2x.png";
+import LaunchImageLg from "./launch-lg.png";
 
-export { LaunchImage };
+export { LaunchImage, LaunchImageLg, LaunchImage2x };
