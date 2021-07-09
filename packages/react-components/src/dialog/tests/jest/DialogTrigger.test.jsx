2a236a1157c89495459a9c6414d4d0e5cfa9d9df
patricklafrance <pa.lafrance@gmail.com>
2021-07-09 16:22:04 -0400

    Finishing up DialogTrigger jest tests

--- packages/react-components/src/dialog/tests/jest/DialogTrigger.test.jsx
+++ packages/react-components/src/dialog/tests/jest/DialogTrigger.test.jsx
@@ -8,11 +8,6 @@ import { act, fireEvent, render, waitFor } from "@testing-library/react";
 import { createRef, forwardRef } from "react";
 import userEvent from "@testing-library/user-event";
 
-/*
-Refs
-- classic tests
-*/
-
 beforeAll(() => {
     Transition.disableAnimation = true;
 });
@@ -357,4 +352,61 @@ test("call onOpenChange when the close function is called", async () => {
 
 // ***** Refs *****
 
-// -> not sure why the ref is not on the overlay, compare with others
+test("ref is a DOM element", async () => {
+    const ref = createRef();
+
+    render(
+        <DialogTrigger defaultOpen ref={ref}>
+            <Button>Trigger</Button>
+            <Dialog>
+                <Heading>Iconic Arecibo Observatory collapses</Heading>
+                <Content>This year, the National Science Foundation (NSF) said farewell to the iconic Arecibo Observatory in Puerto Rico after two major cable failures led to the radio telescope's collapse.</Content>
+            </Dialog>
+        </DialogTrigger>
+    );
+
+    await waitFor(() => expect(ref.current).not.toBeNull());
+
+    await waitFor(() => expect(ref.current instanceof HTMLElement).toBeTruthy());
+    await waitFor(() => expect(ref.current.tagName).toBe("DIV"));
+});
+
+test("when using a callback ref, ref is a DOM element", async () => {
+    let refNode = null;
+
+    render(
+        <DialogTrigger
+            defaultOpen
+            ref={node => {
+                refNode = node;
+            }}
+        >
+            <Button>Trigger</Button>
+            <Dialog>
+                <Heading>Iconic Arecibo Observatory collapses</Heading>
+                <Content>This year, the National Science Foundation (NSF) said farewell to the iconic Arecibo Observatory in Puerto Rico after two major cable failures led to the radio telescope's collapse.</Content>
+            </Dialog>
+        </DialogTrigger>
+    );
+
+    await waitFor(() => expect(refNode).not.toBeNull());
+
+    await waitFor(() => expect(refNode instanceof HTMLElement).toBeTruthy());
+    await waitFor(() => expect(refNode.tagName).toBe("DIV"));
+});
+
+test("set ref once", async () => {
+    const handler = jest.fn();
+
+    render(
+        <DialogTrigger defaultOpen ref={handler}>
+            <Button>Trigger</Button>
+            <Dialog>
+                <Heading>Iconic Arecibo Observatory collapses</Heading>
+                <Content>This year, the National Science Foundation (NSF) said farewell to the iconic Arecibo Observatory in Puerto Rico after two major cable failures led to the radio telescope's collapse.</Content>
+            </Dialog>
+        </DialogTrigger>
+    );
+
+    await waitFor(() => expect(handler).toHaveBeenCalledTimes(1));
+});
