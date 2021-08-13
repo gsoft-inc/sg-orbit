433ffb6090edd123f2dc232898073510259fb911
patricklafrance <pa.lafrance@gmail.com>
2021-08-13 14:19:12 -0400

    Updated styled system typescript tests to use expectAssignable

--- packages/react-components/src/shared/tests/typescript/useStyledSystem.ts
+++ packages/react-components/src/shared/tests/typescript/useStyledSystem.ts
@@ -1,12 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-/*
-TODO: instead define custom assertions function:
-
-- expectAssignable
-- expectNotAssignable
-*/
-
 import {
     BackgroundColorProp,
     BackgroundPositionProp,
@@ -39,272 +30,218 @@ import {
     ZindexProp
 } from "../../src";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const expectAssignable = <T>(value: T) => {
     // Do nothing, the TypeScript compiler handles this for us
 };
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const expectNotAssignable = <T>(value: any) => {
     // Do nothing, the TypeScript compiler handles this for us
 };
 
-function globalValue() {
-    const supportInherit: GlobalValue = "inherit";
-    const supportInitial: GlobalValue = "initial";
-    const supportRevert: GlobalValue = "revert";
-    const supportUnset: GlobalValue = "unset";
-}
+expectAssignable<GlobalValue>("inherit");
+expectAssignable<GlobalValue>("initial");
+expectAssignable<GlobalValue>("revert");
+expectAssignable<GlobalValue>("unset");
 
-function lengthUnit() {
-    const supportPx: LengthUnit = "1px";
-    const supportEm: LengthUnit = "1em";
-    const supportRem: LengthUnit = "1rem";
-    const supportCh: LengthUnit = "1ch";
-    const supportVw: LengthUnit = "1vw";
-    const supportVh: LengthUnit = "1vh";
-    const supportVmin: LengthUnit = "1vmin";
-    const supportVmax: LengthUnit = "1vmax";
-}
+expectAssignable<LengthUnit>("1px");
+expectAssignable<LengthUnit>("1em");
+expectAssignable<LengthUnit>("1rem");
+expectAssignable<LengthUnit>("1ch");
+expectAssignable<LengthUnit>("1vw");
+expectAssignable<LengthUnit>("1vh");
+expectAssignable<LengthUnit>("1vmin");
+expectAssignable<LengthUnit>("1vmax");
 
-function spaceValue() {
-    const supportUnit: SpaceValue = "1px";
-    const supportOrbitSpace: SpaceValue = 1;
-    const supportGlobaValue: SpaceValue = "inherit";
-}
+expectAssignable<SpaceValue>("1px");
+expectAssignable<SpaceValue>(1);
+expectAssignable<SpaceValue>("inherit");
 
-function lengthShorthand() {
-    const supportSingleUnit: LengthShorthand = "1px";
-    const supportDoubleUnit: LengthShorthand = "1px 1px";
-    const supportTripleUnit: LengthShorthand = "1px 1px 1px";
-    const supportQuadrupleUnit: LengthShorthand = "1px 1px 1px 1px";
-}
+expectAssignable<LengthShorthand>("1px");
+expectAssignable<LengthShorthand>("1px 1px");
+expectAssignable<LengthShorthand>("1px 1px 1px");
+expectAssignable<LengthShorthand>("1px 1px 1px 1px");
 
-function colorExpressionTests() {
-    const supportShortHex: ColorExpression = "#fff";
-    const supportLongHex: ColorExpression = "#ffffff";
-    const supportRgb: ColorExpression = "rgb(255, 255, 255)";
-    const supportRgbPercentage: ColorExpression = "rgb(100%, 100%, 100%)";
-    const supportRgba: ColorExpression = "rgba(255, 255, 255, 1)";
-    const supportRgbaPercentage: ColorExpression = "rgba(100%, 100%, 100%, 1)";
-    const supportHsl: ColorExpression = "hsl(100, 100%, 100%)";
-    const supportHsla: ColorExpression = "hsl(100, 100%, 100%, 1)";
-}
+expectAssignable<ColorExpression>("#fff");
+expectAssignable<ColorExpression>("#ffffff");
+expectAssignable<ColorExpression>("rgb(255, 255, 255)");
+expectAssignable<ColorExpression>("rgb(100%, 100%, 100%)");
+expectAssignable<ColorExpression>("rgba(255, 255, 255, 1)");
+expectAssignable<ColorExpression>("rgba(100%, 100%, 100%, 1)");
+expectAssignable<ColorExpression>("hsl(100, 100%, 100%)");
+expectAssignable<ColorExpression>("hsl(100, 100%, 100%, 1)");
 
-function cssColor() {
-    const supportColorExpression: CssColor = "#fff";
-    const supportNamedColor: CssColor = "white";
-}
+expectAssignable<CssColor>("#fff");
+expectAssignable<CssColor>("white");
 
-function colorValue() {
-    const supportColorExpression: ColorValue = "#fff";
-    const supportNamedColor: ColorValue = "white";
-    const supportOrbitColor: ColorValue = "sunray-1";
-    const supportGlobaValue: ColorValue = "inherit";
-}
+expectAssignable<ColorValue>("#fff");
+expectAssignable<ColorValue>("white");
+expectAssignable<ColorValue>("sunray-1");
+expectAssignable<ColorValue>("inherit");
 
-function backgroundColorProp() {
-    const supportColorExpression: BackgroundColorProp = "#fff";
-    const supportNamedColor: BackgroundColorProp = "white";
-    const supportOrbitColor: BackgroundColorProp = "sunray-1";
-    const supportRole: BackgroundColorProp = "alias-1";
-}
+expectAssignable<BackgroundColorProp>("#fff");
+expectAssignable<BackgroundColorProp>("white");
+expectAssignable<BackgroundColorProp>("sunray-1");
+expectAssignable<BackgroundColorProp>("alias-1");
 
-function backgroundPositionProp() {
-    const supportTop: BackgroundPositionProp = "top";
-    const supportBottom: BackgroundPositionProp = "bottom";
-    const supportLeft: BackgroundPositionProp = "left";
-    const supportRight: BackgroundPositionProp = "right";
-    const supportCenter: BackgroundPositionProp = "center";
-    const supportLeftTop: BackgroundPositionProp = "left-top";
-    const supportLeftBottom: BackgroundPositionProp = "left-bottom";
-    const supportRightTop: BackgroundPositionProp = "right-top";
-    const supportRightBottom: BackgroundPositionProp = "right-bottom";
-}
+expectAssignable<BackgroundPositionProp>("top");
+expectAssignable<BackgroundPositionProp>("bottom");
+expectAssignable<BackgroundPositionProp>("left");
+expectAssignable<BackgroundPositionProp>("right");
+expectAssignable<BackgroundPositionProp>("center");
+expectAssignable<BackgroundPositionProp>("left-top");
+expectAssignable<BackgroundPositionProp>("left-bottom");
+expectAssignable<BackgroundPositionProp>("right-top");
+expectAssignable<BackgroundPositionProp>("right-bottom");
 
-function backgroundSizeClasses() {
-    const supportAuto: BackgroundSizeProp = "auto";
-    const supportCover: BackgroundSizeProp = "cover";
-    const supportContain: BackgroundSizeProp = "contain";
-}
+expectAssignable<BackgroundSizeProp>("top");
+expectAssignable<BackgroundSizeProp>("cover");
+expectAssignable<BackgroundSizeProp>("contain");
 
-function borderColorProp() {
-    const supportColorExpression: BorderColorProp = "#fff";
-    const supportNamedColor: BorderColorProp = "white";
-    const supportOrbitColor: BorderColorProp = "sunray-1";
-    const supportRole: BorderColorProp = "alias-1";
-}
+expectAssignable<BorderColorProp>("#fff");
+expectAssignable<BorderColorProp>("white");
+expectAssignable<BorderColorProp>("sunray-1");
+expectAssignable<BorderColorProp>("alias-1");
 
-function borderRadiusProp() {
-    const support0: BorderRadiusProp = 0;
-    const support1: BorderRadiusProp = 1;
-    const support2: BorderRadiusProp = 2;
-    const support3: BorderRadiusProp = 3;
-    const support4: BorderRadiusProp = 4;
-    const support100: BorderRadiusProp = "100";
-    const supportPill: BorderRadiusProp = "pill";
-}
+expectAssignable<BorderRadiusProp>(0);
+expectAssignable<BorderRadiusProp>(1);
+expectAssignable<BorderRadiusProp>(2);
+expectAssignable<BorderRadiusProp>(3);
+expectAssignable<BorderRadiusProp>(4);
+expectAssignable<BorderRadiusProp>("100");
+expectAssignable<BorderRadiusProp>("pill");
 
-function borderStyleProp() {
-    const supportSolid: BorderStyleProp = "solid";
-    const supportDashed: BorderStyleProp = "dashed";
-    const supportDotted: BorderStyleProp = "dotted";
-    const supportDouble: BorderStyleProp = "double";
-    const supportNone: BorderStyleProp = "none";
-}
+expectAssignable<BorderStyleProp>("solid");
+expectAssignable<BorderStyleProp>("dashed");
+expectAssignable<BorderStyleProp>("dotted");
+expectAssignable<BorderStyleProp>("double");
+expectAssignable<BorderStyleProp>("none");
 
-function boxShadowProp() {
-    const support1: BoxShadowProp = 1;
-    const support2: BoxShadowProp = 2;
-    const support3: BoxShadowProp = 3;
-    const support4: BoxShadowProp = 4;
-    const supportSkim: BoxShadowProp = "alias-skim";
-    const supportLifted: BoxShadowProp = "alias-lifted";
-    const supportRaised: BoxShadowProp = "alias-raised";
-    const supportFloating: BoxShadowProp = "alias-floating";
-}
+expectAssignable<BoxShadowProp>(1);
+expectAssignable<BoxShadowProp>(2);
+expectAssignable<BoxShadowProp>(3);
+expectAssignable<BoxShadowProp>(4);
+expectAssignable<BoxShadowProp>("alias-skim");
+expectAssignable<BoxShadowProp>("alias-lifted");
+expectAssignable<BoxShadowProp>("alias-raised");
+expectAssignable<BoxShadowProp>("alias-floating");
 
-function colorProp() {
-    const supportColorExpression: ColorProp = "#fff";
-    const supportNamedColor: ColorProp = "white";
-    const supportOrbitColor: ColorProp = "sunray-1";
-    const supportRole: ColorProp = "alias-1";
-}
+expectAssignable<ColorProp>("#fff");
+expectAssignable<ColorProp>("white");
+expectAssignable<ColorProp>("sunray-1");
+expectAssignable<ColorProp>("alias-1");
 
-function displayProp() {
-    const supportBlock: DisplayProp = "block";
-    const supportInlineBlock: DisplayProp = "inline-block";
-    const supportInline: DisplayProp = "inline";
-    const supportFlex: DisplayProp = "flex";
-    const supportInlineFlex: DisplayProp = "inline-flex";
-    const supportTable: DisplayProp = "table";
-    const supportInlineTable: DisplayProp = "inline-table";
-    const supportTableCaption: DisplayProp = "table-caption";
-    const supportTableCell: DisplayProp = "table-cell";
-    const supportTableColumn: DisplayProp = "table-column";
-    const supportTableColumnGroup: DisplayProp = "table-column-group";
-    const supportTableFooterGroup: DisplayProp = "table-footer-group";
-    const supportTableHeaderGroup: DisplayProp = "table-header-group";
-    const supportTableRowGroup: DisplayProp = "table-row-group";
-    const supportTableRow: DisplayProp = "table-row";
-    const supportGrid: DisplayProp = "grid";
-    const supportInlineGrid: DisplayProp = "inline-grid";
-    const supportListItem: DisplayProp = "list-item";
-    const supportNone: DisplayProp = "none";
-}
+expectAssignable<DisplayProp>("block");
+expectAssignable<DisplayProp>("inline-block");
+expectAssignable<DisplayProp>("inline");
+expectAssignable<DisplayProp>("flex");
+expectAssignable<DisplayProp>("inline-flex");
+expectAssignable<DisplayProp>("table");
+expectAssignable<DisplayProp>("inline-table");
+expectAssignable<DisplayProp>("table-caption");
+expectAssignable<DisplayProp>("table-cell");
+expectAssignable<DisplayProp>("table-column");
+expectAssignable<DisplayProp>("table-column-group");
+expectAssignable<DisplayProp>("table-footer-group");
+expectAssignable<DisplayProp>("table-header-group");
+expectAssignable<DisplayProp>("table-row-group");
+expectAssignable<DisplayProp>("table-row");
+expectAssignable<DisplayProp>("grid");
+expectAssignable<DisplayProp>("inline-grid");
+expectAssignable<DisplayProp>("list-item");
+expectAssignable<DisplayProp>("none");
 
-function fillProp() {
-    const supportColorExpression: FillProp = "#fff";
-    const supportNamedColor: FillProp = "white";
-    const supportOrbitColor: FillProp = "sunray-1";
-    const supportRole: FillProp = "alias-icon-1";
-}
+expectAssignable<FillProp>("#fff");
+expectAssignable<FillProp>("white");
+expectAssignable<FillProp>("sunray-1");
+expectAssignable<FillProp>("alias-icon-1");
 
-function fontSizeProp() {
-    const support1: FontSizeProp = 1;
-    const support2: FontSizeProp = 2;
-    const support3: FontSizeProp = 3;
-    const support4: FontSizeProp = 4;
-    const support5: FontSizeProp = 5;
-    const support6: FontSizeProp = 6;
-    const support7: FontSizeProp = 7;
-    const support8: FontSizeProp = 8;
-    const support9: FontSizeProp = 9;
-    const supportSubHeadline: FontSizeProp = "subheadline";
-    const supportHeadline: FontSizeProp = "headline";
-}
+expectAssignable<FontSizeProp>(1);
+expectAssignable<FontSizeProp>(2);
+expectAssignable<FontSizeProp>(3);
+expectAssignable<FontSizeProp>(4);
+expectAssignable<FontSizeProp>(5);
+expectAssignable<FontSizeProp>(6);
+expectAssignable<FontSizeProp>(7);
+expectAssignable<FontSizeProp>(8);
+expectAssignable<FontSizeProp>(9);
+expectAssignable<FontSizeProp>("subheadline");
+expectAssignable<FontSizeProp>("headline");
 
-function fontWeightProp() {
-    const support1: FontWeightProp = 1;
-    const support2: FontWeightProp = 2;
-    const support3: FontWeightProp = 3;
-    const support4: FontWeightProp = 4;
-    const support5: FontWeightProp = 5;
-    const support6: FontWeightProp = 6;
-    const support7: FontWeightProp = 7;
-    const support8: FontWeightProp = 8;
-    const support9: FontWeightProp = 9;
-}
+expectAssignable<FontWeightProp>(1);
+expectAssignable<FontWeightProp>(2);
+expectAssignable<FontWeightProp>(3);
+expectAssignable<FontWeightProp>(4);
+expectAssignable<FontWeightProp>(5);
+expectAssignable<FontWeightProp>(6);
+expectAssignable<FontWeightProp>(7);
+expectAssignable<FontWeightProp>(8);
+expectAssignable<FontWeightProp>(9);
 
-function heightProp() {
-    const supportOrbitSpace: HeightProp = 1;
-    const support100: HeightProp = "100%";
-    const supportScreen: HeightProp = "screen";
-    const supportAuto: HeightProp = "auto";
-    const supportMaxContent: HeightProp = "max-content";
-    const supportMinContent: HeightProp = "min-content";
-}
+expectAssignable<HeightProp>(1);
+expectAssignable<HeightProp>("100%");
+expectAssignable<HeightProp>("screen");
+expectAssignable<HeightProp>("auto");
+expectAssignable<HeightProp>("max-content");
+expectAssignable<HeightProp>("min-content");
 
-function lineHeightProp() {
-    const support1: LineHeightProp = 1;
-    const support2: LineHeightProp = 2;
-    const support3: LineHeightProp = 3;
-    const support4: LineHeightProp = 4;
-    const support5: LineHeightProp = 5;
-    const support6: LineHeightProp = 6;
-    const supportNone: LineHeightProp = "none";
-}
+expectAssignable<LineHeightProp>(1);
+expectAssignable<LineHeightProp>(2);
+expectAssignable<LineHeightProp>(3);
+expectAssignable<LineHeightProp>(4);
+expectAssignable<LineHeightProp>(5);
+expectAssignable<LineHeightProp>(6);
+expectAssignable<LineHeightProp>("none");
 
-function maxHeightProp() {
-    const supportOrbitSpace: MaxHeightProp = 1;
-    const support100: MaxHeightProp = "100%";
-    const supportAuto: MaxHeightProp = "auto";
-    const supportMaxContent: MaxHeightProp = "max-content";
-    const supportMinContent: MaxHeightProp = "min-content";
-}
+expectAssignable<MaxHeightProp>(1);
+expectAssignable<MaxHeightProp>("100%");
+expectAssignable<MaxHeightProp>("auto");
+expectAssignable<MaxHeightProp>("max-content");
+expectAssignable<MaxHeightProp>("min-content");
 
-function maxWidthProp() {
-    const supportOrbitSpace: MaxWidthProp = 1;
-    const support100: MaxWidthProp = "100%";
-    const supportAuto: MaxWidthProp = "auto";
-    const supportMaxContent: MaxWidthProp = "max-content";
-    const supportMinContent: MaxWidthProp = "min-content";
-}
+expectAssignable<MaxWidthProp>(1);
+expectAssignable<MaxWidthProp>("100%");
+expectAssignable<MaxWidthProp>("auto");
+expectAssignable<MaxWidthProp>("max-content");
+expectAssignable<MaxWidthProp>("min-content");
 
-function minHeightProp() {
-    const supportOrbitSpace: MinHeightProp = 1;
-    const support100: MinHeightProp = "100%";
-    const supportAuto: MinHeightProp = "auto";
-    const supportMaxContent: MinHeightProp = "max-content";
-    const supportMinContent: MinHeightProp = "min-content";
-}
+expectAssignable<MinHeightProp>(1);
+expectAssignable<MinHeightProp>("100%");
+expectAssignable<MinHeightProp>("auto");
+expectAssignable<MinHeightProp>("max-content");
+expectAssignable<MinHeightProp>("min-content");
 
-function minWidthProp() {
-    const supportOrbitSpace: MinWidthProp = 1;
-    const support100: MinWidthProp = "100%";
-    const supportAuto: MinWidthProp = "auto";
-    const supportMaxContent: MinWidthProp = "max-content";
-    const supportMinContent: MinWidthProp = "min-content";
-}
+expectAssignable<MinWidthProp>(1);
+expectAssignable<MinWidthProp>("100%");
+expectAssignable<MinWidthProp>("auto");
+expectAssignable<MinWidthProp>("max-content");
+expectAssignable<MinWidthProp>("min-content");
 
-function positionProp() {
-    const supportStatic: PositionProp = "static";
-    const supportFixed: PositionProp = "fixed";
-    const supportAbsolute: PositionProp = "absolute";
-    const supportRelative: PositionProp = "relative";
-    const supportSticky: PositionProp = "sticky";
-}
+expectAssignable<PositionProp>("static");
+expectAssignable<PositionProp>("fixed");
+expectAssignable<PositionProp>("absolute");
+expectAssignable<PositionProp>("relative");
+expectAssignable<PositionProp>("sticky");
 
-function widthProp() {
-    const supportOrbitSpace: WidthProp = 1;
-    const support100: WidthProp = "100%";
-    const supportScreen: WidthProp = "screen";
-    const supportAuto: WidthProp = "auto";
-    const supportMaxContent: WidthProp = "max-content";
-    const supportMinContent: WidthProp = "min-content";
-}
+expectAssignable<WidthProp>(1);
+expectAssignable<WidthProp>("100%");
+expectAssignable<WidthProp>("screen");
+expectAssignable<WidthProp>("auto");
+expectAssignable<WidthProp>("max-content");
+expectAssignable<WidthProp>("min-content");
 
-function zIndexProp() {
-    const support0: ZindexProp = 0;
-    const support1: ZindexProp = 1;
-    const support2: ZindexProp = 2;
-    const support3: ZindexProp = 3;
-    const support4: ZindexProp = 4;
-    const support5: ZindexProp = 5;
-    const support999: ZindexProp = "999";
-    const support9999: ZindexProp = "9999";
-    const supportMax: ZindexProp = "max";
-}
+expectAssignable<ZindexProp>(0);
+expectAssignable<ZindexProp>(1);
+expectAssignable<ZindexProp>(2);
+expectAssignable<ZindexProp>(3);
+expectAssignable<ZindexProp>(4);
+expectAssignable<ZindexProp>(5);
+expectAssignable<ZindexProp>("999");
+expectAssignable<ZindexProp>("9999");
+expectAssignable<ZindexProp>("max");
 
-const styleProps: StyleProps = {
+expectAssignable<StyleProps>({
     className: "red-border",
     style: {
         border: "1px solid red"
@@ -361,6 +298,6 @@ const styleProps: StyleProps = {
     top: 1,
     width: 1,
     zIndex: 1
-};
+});
 
 
