cbb793d2bd9bba13f19d568c4a6228c00cc8c18b
Francis Thibault <francis.thibault@gsoft.com>
2021-09-16 14:48:51 -0400

    moved normalize to html components

--- packages/react-components/src/html/src/html.tsx
+++ packages/react-components/src/html/src/html.tsx
@@ -2,6 +2,8 @@ import { Box } from "../../box";
 import { ElementRef, forwardRef } from "react";
 import { JsxElement, StyledComponentProps, mergeProps, useStyleProps } from "../../shared";
 
+import "./html.css";
+
 function htmlElement<T extends JsxElement<any>>(name: string, elementType: T, className?: string) {
     return forwardRef<ElementRef<T>, StyledComponentProps<T>>((props, ref) => {
         const [styleProps] = useStyleProps<StyledComponentProps<T>>(name);
@@ -24,56 +26,56 @@ function htmlElement<T extends JsxElement<any>>(name: string, elementType: T, cl
 
 // Sectioning & Content sectioning
 
-export const Address = htmlElement("address", "address");
+export const Address = htmlElement("address", "address", "o-ui-html-address");
 
-export const Article = htmlElement("article", "article");
+export const Article = htmlElement("article", "article", "o-ui-html-article");
 
-export const Aside = htmlElement("aside", "aside");
+export const Aside = htmlElement("aside", "aside", "o-ui-html-aside");
 
 export const Body = htmlElement("body", "body");
 
-export const HtmlFooter = htmlElement("html-footer", "footer");
+export const HtmlFooter = htmlElement("html-footer", "footer", "o-ui-html-footer");
 
-export const HtmlHeader = htmlElement("html-header", "header");
+export const HtmlHeader = htmlElement("html-header", "header", "o-ui-html-header");
 
-export const Main = htmlElement("main", "main");
+export const Main = htmlElement("main", "main", "o-ui-html-main");
 
-export const Nav = htmlElement("nav", "nav");
+export const Nav = htmlElement("nav", "nav", "o-ui-html-nav");
 
-export const Section = htmlElement("section", "section");
+export const Section = htmlElement("section", "section", "o-ui-html-section");
 
 // Text content
 
-export const Div = htmlElement("div", "div");
+export const Div = htmlElement("div", "div", "o-ui-html-div");
 
-export const UL = htmlElement("ul", "ul");
-export const OL = htmlElement("ol", "ol");
-export const LI = htmlElement("li", "li");
+export const UL = htmlElement("ul", "ul", "o-ui-html-ul");
+export const OL = htmlElement("ol", "ol", "o-ui-html-ol");
+export const LI = htmlElement("li", "li", "o-ui-html-li");
 
 // Inline text semantics
 
-export const A = htmlElement("a", "a");
+export const A = htmlElement("a", "a", "o-ui-html-a");
 
-export const Span = htmlElement("span", "span");
+export const Span = htmlElement("span", "span", "o-ui-html-span");
 
 // Table content
 
-export const Table = htmlElement("table", "table");
+export const Table = htmlElement("table", "table", "o-ui-html-table");
 
-export const THead = htmlElement("thead", "thead");
-export const TBody = htmlElement("tbody", "tbody");
-export const TFoot = htmlElement("tfoot", "tfoot");
+export const THead = htmlElement("thead", "thead", "o-ui-html-thead");
+export const TBody = htmlElement("tbody", "tbody", "o-ui-html-tbody");
+export const TFoot = htmlElement("tfoot", "tfoot", "o-ui-html-tfoot");
 
-export const TH = htmlElement("th", "th");
-export const TR = htmlElement("tr", "tr");
-export const TD = htmlElement("td", "td");
+export const TH = htmlElement("th", "th", "o-ui-html-th");
+export const TR = htmlElement("tr", "tr", "o-ui-html-tr");
+export const TD = htmlElement("td", "td", "o-ui-html-td");
 
 // Forms
 
-export const HtmlButton = htmlElement("html-button", "button");
+export const HtmlButton = htmlElement("html-button", "button", "o-ui-html-button");
 
-export const HtmlInput = htmlElement("html-input", "input");
+export const HtmlInput = htmlElement("html-input", "input", "o-ui-html-input");
 
-export const HtmlLabel = htmlElement("html-label", "label");
+export const HtmlLabel = htmlElement("html-label", "label", "o-ui-html-label");
 
-export const HtmlTextArea = htmlElement("html-textarea", "textarea");
+export const HtmlTextArea = htmlElement("html-textarea", "textarea", "o-ui-html-textarea");
