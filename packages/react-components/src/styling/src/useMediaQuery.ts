d1b02ee7f7aec3ebf0898a93ca1c3f1f91c445df
patricklafrance <pa.lafrance@gmail.com>
2021-10-14 16:41:20 -0400

    More sizing scale

--- packages/react-components/src/styling/src/useMediaQuery.ts
+++ packages/react-components/src/styling/src/useMediaQuery.ts
@@ -1,8 +1,7 @@
 import { isFunction } from "../../shared";
 import { useEffect, useState } from "react";
 
-// This check is the make sure that the matchMedia function exists. In the test environement, that
-// function is not available.
+// Ensure that matchMedia function exists. In a jest environement, this function is not available.
 export const supportsMatchMedia = isFunction(window?.matchMedia);
 
 // Copied from https://github.com/adobe/react-spectrum/blob/main/packages/%40react-spectrum/utils/src/useMediaQuery.ts
