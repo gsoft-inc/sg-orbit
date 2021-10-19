90e33fda985829e939f320b1318d6b585519bf89
Francis Thibault <francis.thibault@gmail.com>
2021-10-18 21:54:29 -0400

    removed scaling in tests for images

--- packages/react-components/src/illustration/tests/chromatic/Illustration.chroma.jsx
+++ packages/react-components/src/illustration/tests/chromatic/Illustration.chroma.jsx
@@ -15,15 +15,15 @@ function stories(segment) {
 stories()
     .add("default", () =>
         <Illustration>
-            <Image src={Nasa} alt="Nasa" width={11} />
+            <Image src={Nasa} alt="Nasa" width="150px" />
         </Illustration>
     )
     .add("horizontal", () =>
         <Stack>
-            <Illustration orientation="horizontal" width={17} height={12} backgroundColor="primary-2">
+            <Illustration orientation="horizontal" width="700px" height="200px" backgroundColor="primary-2">
                 <Image src={Nasa} alt="Nasa" />
             </Illustration>
-            <Box width={17} height={12}>
+            <Box width="700px" height="200px">
                 <Illustration orientation="horizontal" backgroundColor="primary-2">
                     <Image src={Nasa} alt="Nasa" />
                 </Illustration>
@@ -32,10 +32,10 @@ stories()
     )
     .add("vertical", () =>
         <Inline>
-            <Illustration orientation="vertical" width={12} height={16} backgroundColor="primary-2">
+            <Illustration orientation="vertical" width="200px" height="500px" backgroundColor="primary-2">
                 <Image src={Nasa} alt="Nasa" />
             </Illustration>
-            <Box width={12} height={16}>
+            <Box width="200px" height="500px">
                 <Illustration orientation="vertical" backgroundColor="primary-2">
                     <Image src={Nasa} alt="Nasa" />
                 </Illustration>
@@ -43,30 +43,30 @@ stories()
         </Inline>
     )
     .add("straight", () =>
-        <Illustration shape="straight" width={17} height={12} backgroundColor="primary-2">
+        <Illustration shape="straight" width="700px" height="200px" backgroundColor="primary-2">
             <Image src={Nasa} alt="Nasa" />
         </Illustration>
     )
     .add("rounded", () =>
-        <Illustration shape="rounded" width={17} height={12} backgroundColor="primary-2">
+        <Illustration shape="rounded" width="700px" height="200px" backgroundColor="primary-2">
             <Image src={Nasa} alt="Nasa" />
         </Illustration>
     )
     .add("color", () =>
         <Stack>
             <Inline>
-                <Illustration backgroundColor="primary-2" width={17} height={12}>
+                <Illustration backgroundColor="primary-2" width="700px" height="200px">
                     <Image src={Nasa} alt="Nasa" />
                 </Illustration>
-                <Illustration backgroundColor="rgb(151, 231, 222)" width={17} height={12}>
+                <Illustration backgroundColor="rgb(151, 231, 222)" width="700px" height="200px">
                     <Image src={Nasa} alt="Nasa" />
                 </Illustration>
             </Inline>
             <Inline>
-                <Illustration backgroundColor="hsla(173, 63%, 75%, 1)" width={17} height={12}>
+                <Illustration backgroundColor="hsla(173, 63%, 75%, 1)" width="700px" height="200px">
                     <Image src={Nasa} alt="Nasa" />
                 </Illustration>
-                <Illustration backgroundColor="#97e7de" width={17} height={12}>
+                <Illustration backgroundColor="#97e7de" width="700px" height="200px">
                     <Image src={Nasa} alt="Nasa" />
                 </Illustration>
             </Inline>
@@ -89,13 +89,13 @@ stories()
     )
     .add("styling", () =>
         <Stack>
-            <Illustration border="sunray-10" width={17} height={12}>
+            <Illustration border="sunray-10" width="700px" height="200px">
                 <Image src={Nasa} alt="Nasa" />
             </Illustration>
-            <Illustration className="border-red" width={17} height={12}>
+            <Illustration className="border-red" width="700px" height="200px">
                 <Image src={Nasa} alt="Nasa" />
             </Illustration>
-            <Illustration style={{ border: "1px solid red" }} width={17} height={12}>
+            <Illustration style={{ border: "1px solid red" }} width="700px" height="200px">
                 <Image src={Nasa} alt="Nasa" />
             </Illustration>
         </Stack>
--- packages/react-components/src/image/tests/chromatic/Image.chroma.jsx
+++ packages/react-components/src/image/tests/chromatic/Image.chroma.jsx
@@ -15,68 +15,68 @@ stories()
         <Image src={Launch} alt="SpaceX launch" />
     )
     .add("contained", () =>
-        <Div width={12} height={12}>
+        <Div width="200px" height="200px">
             <Image src={Launch} alt="SpaceX launch" />
         </Div>
     )
     .add("size", () =>
         <Stack>
-            <Image width={12} src={Launch} alt="SpaceX launch" />
-            <Image width={12} height={12} src={Launch} alt="SpaceX launch" />
+            <Image width="200px" src={Launch} alt="SpaceX launch" />
+            <Image width="200px" height="200px" src={Launch} alt="SpaceX launch" />
         </Stack>
     )
     .add("straight", () =>
         <Inline alignY="end">
-            <Image shape="straight" width={7} height={7} src={Launch} alt="SpaceX launch" />
-            <Image shape="straight" width={10} height={10} src={Launch} alt="SpaceX launch" />
-            <Image shape="straight" width={12} height={12} src={Launch} alt="SpaceX launch" />
-            <Image shape="straight" width={14} height={14} src={Launch} alt="SpaceX launch" />
+            <Image shape="straight" width="50px" height="50px" src={Launch} alt="SpaceX launch" />
+            <Image shape="straight" width="100px" height="100px" src={Launch} alt="SpaceX launch" />
+            <Image shape="straight" width="200px" height="200px" src={Launch} alt="SpaceX launch" />
+            <Image shape="straight" width="300px" height="300px" src={Launch} alt="SpaceX launch" />
             <Image shape="straight" src={Launch} alt="SpaceX launch" />
         </Inline>
     )
     .add("rounded", () =>
         <Inline alignY="end">
-            <Image shape="rounded" width={7} height={7} src={Launch} alt="SpaceX launch" />
-            <Image shape="rounded" width={10} height={10} src={Launch} alt="SpaceX launch" />
-            <Image shape="rounded" width={12} height={12} src={Launch} alt="SpaceX launch" />
-            <Image shape="rounded" width={14} height={14} src={Launch} alt="SpaceX launch" />
+            <Image shape="rounded" width="50px" height="50px" src={Launch} alt="SpaceX launch" />
+            <Image shape="rounded" width="100px" height="100px" src={Launch} alt="SpaceX launch" />
+            <Image shape="rounded" width="200px" height="200px" src={Launch} alt="SpaceX launch" />
+            <Image shape="rounded" width="300px" height="300px" src={Launch} alt="SpaceX launch" />
             <Image shape="rounded" src={Launch} alt="SpaceX launch" />
         </Inline>
     )
     .add("circular", () =>
         <Inline alignY="end">
-            <Image shape="circular" width={7} height={7} src={Launch} alt="SpaceX launch" />
-            <Image shape="circular" width={10} height={10} src={Launch} alt="SpaceX launch" />
-            <Image shape="circular" width={12} height={12} src={Launch} alt="SpaceX launch" />
-            <Image shape="circular" width={14} height={14} src={Launch} alt="SpaceX launch" />
+            <Image shape="circular" width="50px" height="50px" src={Launch} alt="SpaceX launch" />
+            <Image shape="circular" width="100px" height="100px" src={Launch} alt="SpaceX launch" />
+            <Image shape="circular" width="200px" height="200px" src={Launch} alt="SpaceX launch" />
+            <Image shape="circular" width="300px" height="300px" src={Launch} alt="SpaceX launch" />
             <Image shape="circular" src={Launch} alt="SpaceX launch" />
         </Inline>
     )
     .add("object fit", () =>
         <Inline>
-            <Div width={12} height={12}>
+            <Div width="200px" height="200px">
                 <Image objectFit="fill" width="100%" height="100%" src={Launch} alt="SpaceX launch" />
             </Div>
-            <Div width={12} height={12}>
+            <Div width="200px" height="200px">
                 <Image objectFit="contain" width="100%" height="100%" src={Launch} alt="SpaceX launch" />
             </Div>
-            <Div width={12} height={12}>
+            <Div width="200px" height="200px">
                 <Image objectFit="cover" width="100%" height="100%" src={Launch} alt="SpaceX launch" />
             </Div>
-            <Div width={12} height={12}>
+            <Div width="200px" height="200px">
                 <Image objectFit="scale-down" width="100%" height="100%" src={Launch} alt="SpaceX launch" />
             </Div>
-            <Div width={12} height={12}>
+            <Div width="200px" height="200px">
                 <Image objectFit="none" width="100%" height="100%" src={Launch} alt="SpaceX launch" />
             </Div>
         </Inline>
     )
     .add("object position", () =>
         <Inline>
-            <Image objectPosition="50% 50%" fit="none" width={12} height={12} src={Mars} alt="SpaceX launch" />
-            <Image objectPosition="right top" fit="none" width={12} height={12} src={Mars} alt="SpaceX launch" />
-            <Image objectPosition="left bottom" fit="none" width={12} height={12} src={Mars} alt="SpaceX launch" />
-            <Image objectPosition="27 17" fit="none" width={12} height={12} src={Mars} alt="SpaceX launch" />
+            <Image objectPosition="50% 50%" fit="none" width="200px" height="200px" src={Mars} alt="SpaceX launch" />
+            <Image objectPosition="right top" fit="none" width="200px" height="200px" src={Mars} alt="SpaceX launch" />
+            <Image objectPosition="left bottom" fit="none" width="200px" height="200px" src={Mars} alt="SpaceX launch" />
+            <Image objectPosition="27 17" fit="none" width="200px" height="200px" src={Mars} alt="SpaceX launch" />
         </Inline>
     )
     .add("zoom", () =>
