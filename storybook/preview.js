4fdfc2a3e429503be4a149a13678a058232cc235
Francis Thibault <francis.thibault@gmail.com>
2021-10-27 21:26:46 -0400

    alias colors setup

--- storybook/preview.js
+++ storybook/preview.js
@@ -32,7 +32,7 @@ export const parameters = {
                     "Supported Platforms",
                     "Contributing"
                 ],
-                "Materials", ["Colors", "Semantic Colors", "Icons", "Shadows", "Spacing", "Sizing", "Typography", "Motion"],
+                "Materials", ["Colors", "Icons", "Shadows", "Spacing", "Sizing", "Typography", "Motion"],
                 "Layout",
                 "Components",
                 "Content",
