ef74cdcbdce2fc7162a2bcc749a5a09b104a3b19
Francis Thibault <francis.thibault@gmail.com>
2021-11-26 11:44:49 -0500

    box shadow artifacts fix - removed theme in doc

--- docs/getting-started/styling/ThemeSpecificationTable.jsx
+++ docs/getting-started/styling/ThemeSpecificationTable.jsx
@@ -7,7 +7,7 @@ const propTypes = {
 
 const ScaleLinks = {
     "box-shadow-scale": <Link href="?path=/story/shadows--page">1..4 / alias</Link>,
-    "color-scale": <Link href="?path=/story/colors--page">[color]-[1..10] / alias</Link>,
+    "color-scale": <Link href="?path=/story/color-palette--page">[color]-[1..10] / alias</Link>,
     "sizing-scale": <Link href="?path=/story/sizing--page">1..18</Link>,
     "spacing-scale": <Link href="?path=/story/sizing--page">1..13</Link>,
 }
