ed58098490177ea5987d1874e89c1433d6f88f7b
LaurentJouanneau <laurent.jouanneau@gsoft.com>
2022-01-21 11:08:48 -0500

    First exploration into a solve for the Popover issue with trapfocus

--- packages/components/src/overlay/src/useTrapFocus.ts
+++ packages/components/src/overlay/src/useTrapFocus.ts
@@ -8,6 +8,7 @@ export function useTrapFocus(focusManager: FocusManager, { isDisabled }: UseTrap
     const [focusedElementRef, setFocusedElement] = useRefState<HTMLElement>();
 
     const handleKeyDown = useEventCallback((event: KeyboardEvent) => {
+        if (focusManager.trapFocusList.length > 0) { return; }
         if (event.key === Keys.tab) {
             const currentActiveElement = event.target;
 
@@ -20,9 +21,11 @@ export function useTrapFocus(focusManager: FocusManager, { isDisabled }: UseTrap
                 if (event.shiftKey) {
                     const element = focusManager.focusPrevious({ tabbableOnly: true });
                     setFocusedElement(element);
+                    focusManager.addTrapFocusToList(element);
                 } else {
                     const element = focusManager.focusNext({ tabbableOnly: true });
                     setFocusedElement(element);
+                    focusManager.addTrapFocusToList(element);
                 }
             }
         }
@@ -31,6 +34,7 @@ export function useTrapFocus(focusManager: FocusManager, { isDisabled }: UseTrap
     // If a focus event occurs outside the scope (e.g. user tabs from browser location bar),
     // restore focus to the previously focused node or the first tabbable element in the active scope.
     const handleFocus = useEventCallback((event: FocusEvent) => {
+        if (focusManager.trapFocusList.length > 0) { return; }
         const target = event.target as HTMLElement;
 
         if (!focusManager.isInScope(target, { includeChildScopes: true })) {
@@ -39,11 +43,14 @@ export function useTrapFocus(focusManager: FocusManager, { isDisabled }: UseTrap
             } else {
                 const element = focusManager.focusFirst({ tabbableOnly: true });
                 setFocusedElement(element);
+                focusManager.addTrapFocusToList(element);
             }
         } else {
             setFocusedElement(target);
+            focusManager.addTrapFocusToList(target);
         }
     });
+
 
     // Using the capture phrase for the keydown event listener to ensure this hook catch a tab keydown event before the useRestoreFocus hook keydown handler.
     useDocumentListener("keydown", handleKeyDown, !isDisabled, { capture: true });
--- packages/components/src/shared/src/useFocusManager.ts
+++ packages/components/src/shared/src/useFocusManager.ts
@@ -24,6 +24,7 @@ export interface FocusManagerIterationOptions {
 }
 
 export interface FocusManager {
+    addTrapFocusToList: (element: HTMLElement) => void;
     focusFirst: (options?: FocusManagerScopeOptions & FocusManagerIterationOptions & FocusManagerHandlers) => HTMLElement;
     focusFirstQueryMatch: (query: string, handlers?: FocusManagerScopeOptions & FocusManagerHandlers) => HTMLElement;
     focusKey: (key: string, options?: FocusManagerScopeOptions & FocusManagerHandlers) => HTMLElement;
@@ -33,17 +34,20 @@ export interface FocusManager {
     focusTarget: (key: string, options?: FocusManagerScopeOptions & FocusManagerIterationOptions & FocusManagerHandlers) => HTMLElement;
     getActiveElementIndex: (options?: FocusManagerScopeOptions) => number;
     isInScope: (element: HTMLElement, options?: FocusManagerScopeOptions) => boolean;
+    trapFocusList: HTMLElement[];
 }
 
 abstract class FocusManagerBase {
     protected scope: FocusScope;
     protected keyProp: string;
     protected onFocus: (element: HTMLElement) => void;
+    trapFocusList: HTMLElement[];
 
     constructor(scope: FocusScope, { keyProp, onFocus }: FocusManagerOptions = {}) {
         this.scope = scope;
         this.keyProp = keyProp;
         this.onFocus = onFocus;
+        this.trapFocusList = [];
     }
 
     isInScope(element: HTMLElement, options?: FocusManagerScopeOptions) {
@@ -53,6 +57,10 @@ abstract class FocusManagerBase {
     abstract getActiveElementIndex(options?: FocusManagerScopeOptions);
 
     abstract focusElement(element: HTMLElement, handlers: FocusManagerHandlers);
+
+    addTrapFocusToList(element: HTMLElement) {
+        this.trapFocusList.push(element);
+    }
 
     focusFirst({ canFocus, onFocus, onNotFound, ...scopeOptions }: FocusManagerScopeOptions & FocusManagerIterationOptions & FocusManagerHandlers = {}) {
         const iterator = new FocusScopeIterator(this.scope, scopeOptions);
