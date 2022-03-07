4f98fc5cd641efbcf7b806e0cf89424cb098374a
Alexandre Asselin <alexandre.asselin@gsoft.com>
2022-03-07 11:22:08 -0500

    add ssr protection to the boundary element

--- packages/components/src/overlay/src/useOverlayPosition.ts
+++ packages/components/src/overlay/src/useOverlayPosition.ts
@@ -1,6 +1,6 @@
 import { Instance, Placement, createPopper } from "@popperjs/core";
 import { ResponsiveProp } from "../../styling";
-import { isNil, useMergedRefs, useRefState } from "../../shared";
+import { isBrowser, isNil, useMergedRefs, useRefState } from "../../shared";
 import { useCallback } from "react";
 
 export type OverlayPosition = Placement;
@@ -45,7 +45,7 @@ export function useOverlayPosition({
             enabled: allowFlip,
             name: "flip",
             options: {
-                boundary: boundaryElement ?? document.body
+                boundary: boundaryElement ?? (isBrowser ? document.body : undefined)
             }
         });
 
@@ -53,7 +53,7 @@ export function useOverlayPosition({
             enabled: allowPreventOverflow,
             name: "preventOverflow",
             options: {
-                boundary: boundaryElement ?? document.body
+                boundary: boundaryElement ?? (isBrowser ? document.body : undefined)
             }
         });
 
