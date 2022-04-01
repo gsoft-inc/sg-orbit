2aa4f5be8d5be1e5839553564b0acb8e87fc0204
Alexandre Asselin <alexandre.asselin@gsoft.com>
2022-04-01 14:43:16 -0400

    ajust svgo configuration

--- packages/icons/scripts/svgo-config.js
+++ packages/icons/scripts/svgo-config.js
@@ -15,7 +15,6 @@ module.exports = {
                 }
             }
         },
-        { name: "removeXMLNS" },
         { name: "convertStyleToAttrs" },
         { name: "sortAttrs" },
         { name: "removeStyleElement" },
