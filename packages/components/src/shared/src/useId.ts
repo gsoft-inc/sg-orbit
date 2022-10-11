a657988114952e2034da18e4a011733542d79279
Tommy Josépovic <tommy.josepovic@gsoft.com>
2022-10-11 14:03:37 -0400

    Fix useId to work on React 17, but not in SSR

--- packages/components/src/shared/src/useId.ts
+++ packages/components/src/shared/src/useId.ts
@@ -1,17 +1,24 @@
 import { isNil } from "./assertions";
-import { useId as useAutoId, useState } from "react";
+import { useId as useIdReact, useState } from "react";
 
-// Polyfill to support React versions below 18.0.0
+// Polyfill to support useId on React versions below 18.0.0, doesn't work on server
 let currentId = 0;
+const isSSR = !(typeof window !== "undefined" && window.document && window.document.createElement);
 const useIdPolyfill = () => {
     const [id] = useState(() => (":" + currentId++));
+
+    if (isSSR) {
+        return undefined;
+    }
 
     return id;
 };
 
+const useAutoId = useIdReact ?? useIdPolyfill;
+
 export function useId(userId?: string, prefix?: string) {
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    const uuid = useAutoId ? useAutoId() : useIdPolyfill();
+    const uuid = useAutoId();
 
     if (!isNil(userId)) {
         return userId;
