989d4a5a099a94060d711c59b7f1eaf0bc62ac5c
Francis Thibault <francis.thibault@gmail.com>
2022-10-11 13:36:59 -0400

    added star and outline icons

---
+++ packages/components/src/icons/src/generated-icons/StarIcon32.tsx
@@ -0,0 +1,13 @@
+/*
+* THIS FILE IS GENERATED. DO NOT EDIT IT.
+* To re-generate the icons, run the yarn generate-icons-components command in the components project.
+*/
+
+/* eslint-disable */
+import { SVGProps, Ref, forwardRef } from "react";
+
+const InnerStarIcon32 = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 32 32" ref={ref} {...props}><path d="M16 6c.344 0 .659.376.811.697l2.6 5.466 5.814.876c.341.052.624.3.73.639a.964.964 0 0 1-.228.962l-4.207 4.255.993 6.007a.956.956 0 0 1-.36.919.878.878 0 0 1-.953.071L16 23.056l-5.2 2.836a.878.878 0 0 1-.953-.071.956.956 0 0 1-.36-.919l.993-6.007-4.207-4.255a.964.964 0 0 1-.229-.962.915.915 0 0 1 .73-.639l5.815-.876 2.6-5.466C15.34 6.377 15.654 6 16 6Z" /></svg>;
+
+const ForwardRef = forwardRef(InnerStarIcon32);
+export { ForwardRef as ReactComponent };
+/* eslint-enable */
