1f5a23137eda575063eb744d8e39ddee41638a67
Francis Thibault <francis.thibault@gmail.com>
2022-10-20 15:58:52 -0400

    convertion of 3 tests to CSF

--- packages/components/src/dot/tests/chromatic/Dot.chroma.jsx
+++ packages/components/src/dot/tests/chromatic/Dot.chroma.jsx
@@ -1,60 +1,57 @@
 import { Div } from "@components/html";
 import { Dot } from "@components/dot";
 import { Inline, Stack } from "@components/layout";
-import { storiesOfBuilder } from "@stories/utils";
 
-function stories(segment) {
-    return storiesOfBuilder(module, "Chromatic/Dot")
-        .segment(segment)
-        .build();
-}
+export default {
+    title: "Chromatic/Dot",
+    component: Dot
+};
 
-stories()
-    .add("default", () =>
+export const Default = () => <Dot color="alias-hard-break" />;
+
+export const Label = () => <Dot color="alias-hard-break">Habitable</Dot>;
+
+export const Color = () => <Stack>
+    <Dot color="hsl(25, 69%, 41%)"></Dot>
+    <Dot color="hsla(25, 69%, 41%, 0.5)"></Dot>
+    <Dot color="#a4b5dd"></Dot>
+    <Dot color="rgb(128,0,0)"></Dot>
+    <Dot color="rgb(128,0,0,0.5)"></Dot>
+    <Dot color="alias-accent"></Dot>
+    <Dot color="red"></Dot>
+    <Dot color="--o-ui-neutral-6"></Dot>
+</Stack>;
+
+export const Disabled = () => <Stack>
+    <Dot disabled color="alias-hard-break"></Dot>
+    <Dot color="hsl(25, 69%, 41%)" disabled>Habitable</Dot>
+</Stack>;
+
+export const Zoom = () => <Inline>
+    <Div className="zoom-in">
         <Dot color="alias-hard-break" />
-    )
-    .add("label", () =>
-        <Dot color="alias-hard-break">Habitable</Dot>
-    )
-    .add("color", () =>
-        <Stack>
-            <Dot color="hsl(25, 69%, 41%)"></Dot>
-            <Dot color="hsla(25, 69%, 41%, 0.5)"></Dot>
-            <Dot color="#a4b5dd"></Dot>
-            <Dot color="rgb(128,0,0)"></Dot>
-            <Dot color="rgb(128,0,0,0.5)"></Dot>
-            <Dot color="alias-accent"></Dot>
-            <Dot color="red"></Dot>
-            <Dot color="--o-ui-neutral-6"></Dot>
-        </Stack>
-    )
-    .add("disabled", () =>
-        <Stack>
-            <Dot disabled color="alias-hard-break"></Dot>
-            <Dot color="hsl(25, 69%, 41%)" disabled>Habitable</Dot>
-        </Stack>
-    )
-    .add("zoom", () =>
-        <Inline>
-            <Div className="zoom-in">
-                <Dot color="alias-hard-break" />
-            </Div>
-            <Div className="zoom-out">
-                <Dot color="alias-hard-break" />
-            </Div>
-        </Inline>
-    )
-    .add("styling", () =>
-        <Stack>
-            <Inline>
-                <Dot border="warning-7" color="alias-hard-break"></Dot>
-                <Dot className="border-red" color="alias-hard-break"></Dot>
-                <Dot style={{ border: "1px solid red" }} color="alias-hard-break"></Dot>
-            </Inline>
-            <Inline>
-                <Dot border="warning-7" color="alias-hard-break">Habitable</Dot>
-                <Dot className="border-red" color="alias-hard-break">Habitable</Dot>
-                <Dot style={{ border: "1px solid red" }} color="alias-hard-break">Habitable</Dot>
-            </Inline>
-        </Stack>
-    );
+    </Div>
+    <Div className="zoom-out">
+        <Dot color="alias-hard-break" />
+    </Div>
+</Inline>;
+
+export const Styling = () => <Stack>
+    <Inline>
+        <Dot border="warning-7" color="alias-hard-break"></Dot>
+        <Dot className="border-red" color="alias-hard-break"></Dot>
+        <Dot style={{ border: "1px solid red" }} color="alias-hard-break"></Dot>
+    </Inline>
+    <Inline>
+        <Dot border="warning-7" color="alias-hard-break">Habitable</Dot>
+        <Dot className="border-red" color="alias-hard-break">Habitable</Dot>
+        <Dot style={{ border: "1px solid red" }} color="alias-hard-break">Habitable</Dot>
+    </Inline>
+</Stack>;
+
+Default.storyName = "default";
+Label.storyName = "label";
+Color.storyName = "color";
+Disabled.storyName = "disabled";
+Zoom.storyName = "zoom";
+Styling.storyName = "styling";
