d5a1c0744373b6f11e27e2890f7765962c2e125c
Alexandre Asselin <alexandre.asselin@gsoft.com>
2022-11-10 10:28:58 -0500

    update stylelint and stylelint config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,19 +2,5 @@ module.exports = {
     extends: "@sharegate/stylelint-config-recommended",
     rules: {
         "linebreaks": "unix",
-        "no-descending-specificity": null,
-        "declaration-colon-space-after": "always-single-line",
-        "unit-allowed-list": [
-            "em",
-            "rem",
-            "px",
-            "%",
-            "fr",
-            "deg",
-            "vh",
-            "vw",
-            "s",
-            "ch"
-        ]
     }
 };
