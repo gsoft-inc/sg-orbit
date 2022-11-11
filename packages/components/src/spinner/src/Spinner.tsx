6d241f4323e45456666085ee4e66a3614485f624
Alexandre Asselin <alexandre.asselin@gsoft.com>
2022-11-11 17:27:39 -0500

    add children property and fix sizes

--- packages/components/src/spinner/src/Spinner.tsx
+++ packages/components/src/spinner/src/Spinner.tsx
@@ -1,22 +1,44 @@
 import { ComponentProps, forwardRef } from "react";
-import { InternalProps, OmitInternalProps, StyledComponentProps, cssModule, mergeProps, normalizeSize } from "../../shared";
+import { isNil, InternalProps, OmitInternalProps, StyledComponentProps, cssModule, mergeProps, normalizeSize, createSizeAdapter } from "../../shared";
 import { Box } from "../../box";
 import { ResponsiveProp, useResponsiveValue } from "../../styling";
-import { VisuallyHidden } from "../../visually-hidden";
+import { Div } from "../../html";
+import { Text } from "../../typography";
 
 const DefaultElement = "div";
 
 export interface InnerSpinnerProps extends InternalProps, StyledComponentProps<typeof DefaultElement> {
-    size?: ResponsiveProp<"2xs" | "xs" | "sm" | "md" | "lg" | "xl" | "2xl">;
+    size?: ResponsiveProp<"sm" | "md" | "lg">;
 }
+
+/* eslint-disable sort-keys, sort-keys-fix/sort-keys-fix */
+const textSize = createSizeAdapter({
+    "sm": "sm",
+    "md": "md",
+    "lg": "lg"
+});
+/* eslint-enable sort-keys, sort-keys-fix/sort-keys-fix */
 
 export function InnerSpinner({
     as = DefaultElement,
     size = "md",
     forwardedRef,
+    children,
+    "aria-label": ariaLabel,
+    "aria-labelledby": ariaLabelledBy,
     ...rest
 }: InnerSpinnerProps) {
     const sizeValue = useResponsiveValue(size);
+
+    if (isNil(children) && isNil(ariaLabel) && isNil(ariaLabelledBy)) {
+        console.error("A spinner must either have children, an \"aria-label\" attribute or an \"aria-labelledby\" attribute.");
+    }
+
+    const labelMarkup = children && (
+        <Text size={textSize(sizeValue)}>
+            {children}
+        </Text>
+    );
 
     return (
         <Box
@@ -24,15 +46,18 @@ export function InnerSpinner({
                 rest,
                 {
                     as,
-                    className: cssModule(
-                        "o-ui-spinner",
-                        normalizeSize(sizeValue)
-                    ),
+                    className: "o-ui-spinner-wrapper",
                     ref: forwardedRef
                 }
             )}
         >
-            <VisuallyHidden>Loading...</VisuallyHidden>
+            <Div
+                className={cssModule(
+                    "o-ui-spinner",
+                    normalizeSize(sizeValue)
+                )}
+            />
+            {labelMarkup}
         </Box>
     );
 }
