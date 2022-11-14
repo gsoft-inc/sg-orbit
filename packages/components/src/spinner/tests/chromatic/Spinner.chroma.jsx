18faa4ba3bca7ec1164f8546221ecdb7868a1119
Alexandre Asselin <alexandre.asselin@gsoft.com>
2022-11-14 12:04:30 -0500

    add chromatic test and fix spinner size when overflowing

---
+++ packages/components/src/spinner/tests/chromatic/Spinner.chroma.jsx
@@ -0,0 +1,64 @@
+import { Spinner } from "@components/spinner";
+import { Inline, Stack } from "@components/layout";
+
+export default {
+    component: Spinner,
+    title: "Chromatic/Spinner"
+};
+
+export const Default = () => (
+    <Inline alignY="end" className="zoom-out">
+        <Spinner size="sm" color="red" ariaLabel="Crawling in progress" />
+        <Spinner size="md" ariaLabel="Crawling in progress" />
+        <Spinner size="lg" ariaLabel="Crawling in progress" />
+    </Inline>
+);
+
+Default.storyName = "default";
+
+export const Styling = () => (
+    <Inline>
+        <Spinner color="red" >Crawling in progress</Spinner>
+        <Spinner className="border-red" />
+        <Spinner style={{ border: "1px solid red" }} />
+    </Inline>
+);
+
+Styling.storyName = "styling";
+
+export const Zoom = () => (
+    <Stack>
+        <Inline alignY="end" className="zoom-in">
+            <Spinner size="sm" color="red">Crawling in progress</Spinner>
+            <Spinner size="md">Crawling in progress</Spinner>
+            <Spinner size="lg">Crawling in progress</Spinner>
+        </Inline>
+        <Inline alignY="end" className="zoom-out">
+            <Spinner size="sm" color="red">Crawling in progress</Spinner>
+            <Spinner size="md">Crawling in progress</Spinner>
+            <Spinner size="lg">Crawling in progress</Spinner>
+        </Inline>
+    </Stack>
+);
+
+Zoom.storyName = "zoom";
+
+export const Label = () => (
+    <Inline alignY="end" >
+        <Spinner size="sm" color="red">Crawling in progress</Spinner>
+        <Spinner size="md">Crawling in progress</Spinner>
+        <Spinner size="lg">Crawling in progress</Spinner>
+    </Inline>
+);
+
+Label.storyName = "label";
+
+export const Overflow = () => (
+    <Stack width={10}>
+        <Spinner size="sm" color="red">Crawling in progress</Spinner>
+        <Spinner size="md">Crawling in progress</Spinner>
+        <Spinner size="lg">Crawling in progress</Spinner>
+    </Stack>
+);
+
+Overflow.storyName = "overflow";
