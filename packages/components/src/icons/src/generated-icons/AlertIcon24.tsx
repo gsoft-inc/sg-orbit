6752160999d979ee29d04e8c7e92a268bbc4c82f
Francis Thibault <francis.thibault@gmail.com>
2022-11-16 15:47:15 -0500

    modified icons to make them bigger and less jaggy

--- packages/components/src/icons/src/generated-icons/AlertIcon24.tsx
+++ packages/components/src/icons/src/generated-icons/AlertIcon24.tsx
@@ -6,7 +6,7 @@
 /* eslint-disable */
 import { SVGProps, Ref, forwardRef } from "react";
 
-const InnerAlertIcon24 = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" viewBox="0 0 24 24" ref={ref} {...props}><path d="M12.354 4.06a.5.5 0 0 0-.708 0l-7.585 7.586a.5.5 0 0 0 0 .707l7.585 7.586a.5.5 0 0 0 .707 0l7.586-7.585a.5.5 0 0 0 0-.708l-7.585-7.585Zm-1.415-.706a1.5 1.5 0 0 1 2.122 0l7.585 7.585a1.5 1.5 0 0 1 0 2.122l-7.585 7.585a1.5 1.5 0 0 1-2.122 0l-7.585-7.585a1.5 1.5 0 0 1 0-2.122l7.585-7.585ZM12 8.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V9a.5.5 0 0 1 .5-.5Zm0 7.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" /></svg>;
+const InnerAlertIcon24 = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" ref={ref} {...props}><path d="M12.605 15.156a.592.592 0 1 1-1.185 0 .592.592 0 0 1 1.185 0Zm-.607-6.949a.569.569 0 0 0-.569.57v3.984a.569.569 0 1 0 1.138 0V8.776a.569.569 0 0 0-.569-.569Z" /><path fillRule="evenodd" d="M10.324 3.694a2.37 2.37 0 0 1 3.352 0l6.63 6.63a2.37 2.37 0 0 1 0 3.352l-6.63 6.63a2.37 2.37 0 0 1-3.352 0l-6.63-6.63a2.37 2.37 0 0 1 0-3.352l6.63-6.63Zm2.682.67a1.422 1.422 0 0 0-2.012 0l-6.63 6.63a1.422 1.422 0 0 0 0 2.012l6.63 6.63a1.422 1.422 0 0 0 2.012 0l6.63-6.63a1.422 1.422 0 0 0 0-2.012l-6.63-6.63Z" clipRule="evenodd" /></svg>;
 
 const ForwardRef = forwardRef(InnerAlertIcon24);
 export { ForwardRef as ReactComponent };
--- packages/components/src/icons/src/generated-icons/AlertIcon32.tsx
+++ packages/components/src/icons/src/generated-icons/AlertIcon32.tsx
@@ -6,7 +6,7 @@
 /* eslint-disable */
 import { SVGProps, Ref, forwardRef } from "react";
 
-const InnerAlertIcon32 = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} fill="none" viewBox="0 0 32 32" ref={ref} {...props}><path d="M16.354 7.06a.5.5 0 0 0-.708 0l-8.585 8.586a.5.5 0 0 0 0 .707l8.585 8.586a.5.5 0 0 0 .707 0l8.586-8.585a.5.5 0 0 0 0-.708l-8.585-8.585Zm-1.415-.706a1.5 1.5 0 0 1 2.122 0l8.585 8.585a1.5 1.5 0 0 1 0 2.122l-8.585 8.585a1.5 1.5 0 0 1-2.122 0l-8.585-8.585a1.5 1.5 0 0 1 0-2.122l8.585-8.585ZM16.5 12v5a.5.5 0 0 1-1 0v-5a.5.5 0 1 1 1 0Zm-.5 8.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" /></svg>;
+const InnerAlertIcon32 = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 32 32" ref={ref} {...props}><path d="M16.97 20.282a1 1 0 1 1-1.999 0 1 1 0 0 1 1.999 0Zm-1.001-9.645a.749.749 0 0 0-.749.75v5.497a.749.749 0 1 0 1.497 0v-5.497a.749.749 0 0 0-.748-.75Z" /><path fillRule="evenodd" d="M17.412 4.585a1.997 1.997 0 0 0-2.824 0L4.585 14.588c-.78.78-.78 2.044 0 2.824l10.003 10.003c.78.78 2.044.78 2.824 0l10.003-10.003c.78-.78.78-2.044 0-2.824L17.412 4.585Zm9.297 10.709L16.706 5.29a.999.999 0 0 0-1.412 0L5.29 15.294a.999.999 0 0 0 0 1.412L15.294 26.71a.999.999 0 0 0 1.412 0L26.71 16.706a.999.999 0 0 0 0-1.412Z" clipRule="evenodd" /></svg>;
 
 const ForwardRef = forwardRef(InnerAlertIcon32);
 export { ForwardRef as ReactComponent };
