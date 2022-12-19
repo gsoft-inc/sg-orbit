bd166d48d8f86a741be94e380b042aed4c093367
Alexandre Asselin <alexandre.asselin@gsoft.com>
2022-12-19 13:57:37 -0500

    change experimental message

--- storybook/components/experimental-message/ExperimentalMessage.jsx
+++ storybook/components/experimental-message/ExperimentalMessage.jsx
@@ -2,20 +2,19 @@ import { Message } from "@components/message";
 import { Content } from "@components/placeholders";
 import { Heading } from "@components/typography";
 
-export function ExperimentalMessage({ noDoc, noTests, noVisualTesting, noFinalDesign, noCompleteSpecs, noMobileSupport }) {
-    const hasDetails = [noDoc, noTests, noVisualTesting, noFinalDesign, noCompleteSpecs, noMobileSupport].some(validation => validation);
+export function ExperimentalMessage({ noDoc, noTests, noVisualTesting, noFinalDesign, noMobileSupport }) {
+    const hasDetails = [noDoc, noTests, noVisualTesting, noFinalDesign, noMobileSupport].some(validation => validation);
 
     return (
         <Message variant="negative">
             <Content>
                 <Heading>Experimental</Heading>
-            This component is in development. There could be breaking changes made to it in a non-major release. Please use with caution
+            This component and its documentation is in development. There could be breaking changes made to it in a non-major release. Please use with caution
                 {hasDetails && <ul>
                     {noDoc && <li>Documentation is missing</li>}
                     {noTests && <li>Tests are missing</li>}
                     {noVisualTesting && <li>Visual testing is missing</li>}
-                    {noFinalDesign && <li>Final design is missing</li>}
-                    {noCompleteSpecs && <li>Complete specs are missing</li>}
+                    {noFinalDesign && <li>The Design is not final</li>}
                     {noMobileSupport && <li>Mobile support is missing</li>}
                 </ul>}
             </Content>
