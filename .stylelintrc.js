1aeab6c7a8cc2c3c3b74f4590f356478d589ba36
Alexandre Asselin <alexandre.asselin@gsoft.com>
2022-12-19 22:35:03 -0500

    update stylelint and config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,16 @@
+/** @type {import('stylelint').Config} */
 module.exports = {
     extends: "@sharegate/stylelint-config-recommended",
     rules: {
         "linebreaks": "unix",
+        "function-comma-space-after": "always",
+        "function-parentheses-space-inside": "never",
+        "function-whitespace-after": "always",
+        "block-opening-brace-space-before": "always",
+        "property-case": "lower",
+        "selector-attribute-operator-space-after": "never",
+        "selector-attribute-operator-space-before": "never",
+        "selector-combinator-space-before": "always",
+        "selector-combinator-space-after": "always",
     }
 };
