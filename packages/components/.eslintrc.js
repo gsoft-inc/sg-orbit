af8c80e33e6f7ec42bb861824e5d84a8b562a8cc
Alexandre Asselin <alexandre.asselin@gsoft.com>
2023-01-16 21:53:59 -0500

    changed errors to warning

--- packages/components/.eslintrc.js
+++ packages/components/.eslintrc.js
@@ -10,16 +10,16 @@ module.exports = {
         {
             files: ["*.ts?(x)"],
             rules: {
-                "react/jsx-sort-props": "error"
+                "react/jsx-sort-props": "warn"
             }
         },
         {
             files: ["**/src/*.ts?(x)"],
             plugins: ["sort-destructure-keys", "sort-keys-fix", "typescript-sort-keys"],
             rules: {
-                "sort-keys-fix/sort-keys-fix": ["error", "asc", { natural: true }],
-                "sort-destructure-keys/sort-destructure-keys": "error",
-                "typescript-sort-keys/interface": ["error", "asc", { natural: true }]
+                "sort-keys-fix/sort-keys-fix": ["warn", "asc", { natural: true }],
+                "sort-destructure-keys/sort-destructure-keys": "warn",
+                "typescript-sort-keys/interface": ["warn", "asc", { natural: true }]
             }
         },
         {
