375213f575c7116c1f9b37f1b0a07c36412be856
Alexandre Asselin <alexandre.asselin@gsoft.com>
2023-02-01 11:45:09 -0500

    add comment for the ignore

--- storybook/components/link/Link.tsx
+++ storybook/components/link/Link.tsx
@@ -8,6 +8,11 @@ export interface LinkProps {
 }
 
 export function Link({ children, target = "", ...rest }: LinkProps) {
-    // @ts-ignore children is not detected, but i'm only converting the code
-    return <AnchorMdx target={target} {...rest}>{children}</AnchorMdx>;
+    return (
+        // @ts-ignore There is an issue with Storybook's typing. Since React.FC dropped the automatic children props, AnchorMdx don't accept a children
+        // in its typings, but it still accepts it as a prop in javascript. So we disable type checking for this line.
+        <AnchorMdx target={target} {...rest}>
+            {children}
+        </AnchorMdx>
+    );
 }
