f0cfdc4a5524bf35a80bf6805a1fc9368f18bc9d
Francis Thibault <francis.thibault@gmail.com>
2023-02-06 13:58:38 -0500

    added missing icon / fix start task error

---
+++ packages/components/src/icons/src/generated-icons/StarFillMinorIcon.tsx
@@ -0,0 +1,11 @@
+/*
+* THIS FILE IS GENERATED. DO NOT EDIT IT.
+* To re-generate the icons, run the yarn generate-icons-components command in the components project.
+*/
+
+/* eslint-disable */
+import { SVGProps, Ref, forwardRef } from "react";
+const InnerStarFillMinorIcon = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" ref={ref} {...props}><path fillRule="evenodd" d="M12 6a.75.75 0 0 1 .715.523L14.04 10h3.21a.75.75 0 0 1 .46 1.342L15 13l.97 4.053c.086.315.002.65-.272.827a.75.75 0 0 1-.884-.051L12 15l-2.733 2.829a.75.75 0 0 1-.884.05c-.274-.177-.449-.564-.363-.879L9 13l-2.64-1.658c-.2-.156-.356-.404-.36-.65a.56.56 0 0 1 .03-.192c.104-.303.47-.5.79-.5h3.22l1.245-3.477A.75.75 0 0 1 12 6Z" clipRule="evenodd" /></svg>;
+const ForwardRef = forwardRef(InnerStarFillMinorIcon);
+export { ForwardRef as ReactComponent };
+/* eslint-enable */
