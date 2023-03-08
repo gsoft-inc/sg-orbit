e56cf1d91c422169be807b25f8c97afc76c638bb
Alexandre Asselin <alexandre.asselin@gsoft.com>
2023-03-08 15:21:10 -0500

    fix test-utils

--- packages/components/src/spinner/tests/jest/Spinner.ssr.test.tsx
+++ packages/components/src/spinner/tests/jest/Spinner.ssr.test.tsx
@@ -3,7 +3,7 @@
  */
 import { Spinner } from "@components/spinner";
 import { renderToString } from "react-dom/server";
-import { throwOnConsoleLogs } from "@jest-utils";
+import { throwOnConsoleLogs } from "@test-utils";
 
 test("can render on the server", () => {
     throwOnConsoleLogs();
