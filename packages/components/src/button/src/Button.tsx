d09876ffb15268fb0bcfa02ea7148db70dc9b57b
Alexandre Asselin <alexandre.asselin@gsoft.com>
2023-03-21 15:16:46 -0400

    update button

--- packages/components/src/button/src/Button.tsx
+++ packages/components/src/button/src/Button.tsx
@@ -168,7 +168,7 @@ export function InnerButton(props: InnerButtonProps) {
                     as: asProp,
                     className: cssModule(
                         "o-ui-button",
-                        "extra-random-class17",
+                        "extra-random-class18",
                         icon && "has-start-icon",
                         endIcon && "has-end-icon"
                     ),
