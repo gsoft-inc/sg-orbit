fcc2f1d41addd6bfc4e4846eb32f0b1378f207e2
Alexandre Asselin <alexandre.asselin@gsoft.com>
2023-03-21 12:10:21 -0400

    reactive more stuff

--- packages/storybook/components/snippet/Snippet.tsx
+++ packages/storybook/components/snippet/Snippet.tsx
@@ -1,7 +1,10 @@
 import "./Snippet.css";
 
-import { ComponentProps } from "react";
-import Highlight from "prism-react-renderer";
+import { Div } from "@components/html";
+import { isNil, mergeClasses } from "@components/shared";
+import { useFormattedCode } from "./useFormattedCode";
+import { ComponentProps, useState } from "react";
+import Highlight, { defaultProps } from "prism-react-renderer";
 import theme from "prism-react-renderer/themes/dracula";
 
 export const CodeTheme = theme;
@@ -11,11 +14,67 @@ interface CodeBlockProps extends Omit<ComponentProps<typeof Highlight>, "childre
     className?: string;
 }
 
+function CodeBlock({ code, language, className: wrapperClassName, ...rest }: CodeBlockProps) {
+    const formattedCode = useFormattedCode(code, language);
+
+    return (
+        <Div
+            className={mergeClasses(
+                "o-ui-sb-snippet",
+                "docblock-source",
+                wrapperClassName
+            )}
+        >
+            <Highlight
+                {...rest}
+                {...defaultProps}
+                code={formattedCode}
+                language={language}
+                theme={CodeTheme}
+            >
+                {({ className, style, tokens, getLineProps, getTokenProps }) => (
+                    <pre className={className} style={style}>
+                        {tokens.map((line, i) => (
+                            <Div {...getLineProps({ line, key: i })}>
+                                {line.map((token, key) => (
+                                    <span {...getTokenProps({ token, key })} />
+                                ))}
+                            </Div>
+                        ))}
+                    </pre>
+                )}
+            </Highlight>
+        </Div>
+    );
+}
+
+interface FileSnippetProps extends Omit<CodeBlockProps, "code"> {
+    filePath: string;
+}
+
+function FileSnippet({ filePath, language, ...rest }: FileSnippetProps) {
+    const [code, setCode] = useState();
+
+    if (isNil(code)) {
+        import(/* webpackMode: "eager" */ `!!raw-loader!@root/packages/components/src${filePath}.sample.jsx`)
+            .then(module => {
+                setCode(module.default);
+            });
+
+        return null;
+    }
+
+    return <CodeBlock code={code} language={language} {...rest} />;
+}
+
 export interface SnippetProps extends CodeBlockProps {
     filePath?: string;
 }
 
 export function Snippet({ code, filePath, language = "jsx", ...rest }: SnippetProps) {
-    return <>{code ?? filePath}</>
-}
+    if (!isNil(filePath)) {
+        return <FileSnippet filePath={filePath} language={language} {...rest} />;
+    }
 
+    return <CodeBlock code={code} language={language} {...rest} />;
+}
