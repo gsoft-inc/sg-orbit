6af53dfabe85e21e55929fa6b40e5d7822a7b480
Alexandre Asselin <alexandre.asselin@gsoft.com>
2023-03-22 13:10:16 -0400

    revert changes to experimental messages

--- .storybook/components/experimental-message/ExperimentalMessage.tsx
+++ .storybook/components/experimental-message/ExperimentalMessage.tsx
@@ -1,20 +1,32 @@
+import { Message, MessageProps } from "@components/message";
+import { Content } from "@components/placeholders";
+import { Heading, Text } from "@components/typography";
 
-export function ExperimentalMessage({ noDoc, noTests, noVisualTesting, noFinalDesign, noMobileSupport, ...rest }: any) {
+interface ExperimentalMessageProps extends MessageProps{
+    noDoc?: boolean;
+    noTests?: boolean;
+    noVisualTesting?: boolean;
+    noFinalDesign?: boolean;
+    noMobileSupport?: boolean;
+}
+
+export function ExperimentalMessage({ noDoc, noTests, noVisualTesting, noFinalDesign, noMobileSupport, ...rest }: ExperimentalMessageProps) {
     const hasDetails = [noDoc, noTests, noVisualTesting, noFinalDesign, noMobileSupport].some(validation => validation);
 
     return (
-        <div>
-                <h1>Experimental</h1>
-                <span>
+        <Message variant="negative" fontSize={4} {...rest}>
+            <Content>
+                <Heading>Experimental</Heading>
+                <Text size="lg">
                     This component and its documentation is in development. There could be breaking changes made to it in a non-major release. Please use with caution
                     {hasDetails && <ul>
                         {noDoc && <li>Documentation is missing</li>}
-                        {noTests && <li>Tests are missing</li>}
-                        {noVisualTesting && <li>Visual testing is missing</li>}
+	@@ -26,8 +14,7 @@ export function ExperimentalMessage({ noDoc, noTests, noVisualTesting, noFinalDe
                         {noFinalDesign && <li>The Design is not final</li>}
                         {noMobileSupport && <li>Mobile support is missing</li>}
                     </ul>}
-                </span>
-        </div>
+                </Text>
+            </Content>
+        </Message>
     );
 }
