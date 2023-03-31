5faf935a49e2ce396d8541394a638dd4f380abac
Alexandre Asselin <alexandre.asselin@gsoft.com>
2023-03-31 16:09:22 -0400

    push new stylelint rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,36 @@
 /** @type {import('stylelint').Config} */
 module.exports = {
-    extends: "@sharegate/stylelint-plugin",
+    extends: ["stylelint-config-standard"],
     rules: {
-        "linebreaks": "unix",
-    }
+        // Avoid Errors
+        "no-descending-specificity": null, // TODO: Disable from recommended
+        "selector-pseudo-class-no-unknown": [ // TODO: dans recommended c'est juste true, on peut tu enlever ça?
+            true,
+            {
+                ignorePseudoClasses: ["global"]
+            }
+        ],
+
+        // Enforce conventions
+        "max-nesting-depth": 2, // TODO: dans aucune config, on le met en extra
+        "declaration-block-single-line-max-declarations": 1, // TODO: dans aucune config, on le met en extra
+        "font-weight-notation": "numeric",  // TODO: dans aucune config, on le met en extra
+        "unit-allowed-list": [ // TODO: dans aucune config, on le met en extra
+            "em",
+            "rem",
+            "px",
+            "%",
+            "fr",
+            "deg",
+            "vh",
+            "vw",
+            "s",
+            "ch"
+        ],
+
+
+
+        "color-hex-length": "long", // standard utilise short... on fait le switch ou on garde l'override?
+        "selector-class-pattern": null, // TODO: Disable from standard. App uses a camelCase convention for class names since we use CSS Modules.
+     }
 };
